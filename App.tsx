eefe9d7fbea660513ba020e77b3157175e7b1247
Grant Cohoe <grant@grantcohoe.com>
2023-02-15 11:59:43 -0500

    temporarily simplify

--- App.tsx
+++ App.tsx
@@ -7,29 +7,18 @@
 
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {LoginView} from './src/components/views/Login';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {ExampleAppView} from "./src/components/views/Example";
+// import {Provider as PaperProvider} from 'react-native-paper';
+// import {ExampleAppView} from "./src/components/views/Example";
 
 function App(): JSX.Element {
-  console.log('Doing the thing with the stacks and stuff');
-  const Stack = createNativeStackNavigator();
+  // console.log('Doing the thing with the stacks and stuff');
+  // const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <PaperProvider>
-        <Stack.Navigator>
-          <Stack.Screen
-            name={'Home'}
-            component={ExampleAppView}
-            options={{title: 'OMG WELCOME TITLE'}}
-          />
-          <Stack.Screen name={'Login'} component={LoginView}/>
-        </Stack.Navigator>
-      </PaperProvider>
-    </NavigationContainer>
+    <LoginView />
   );
 }
 
---
+++ README.md
@@ -0,0 +1,14 @@
+Tricorder
+=========
+
+https://reactnative.dev/docs/environment-setup
+
+```
+nvm install lts/fermium
+nvm use lts/fermium (v14.18.3)
+
+npm install [react-native]
+
+npx react-native start
+npx react-native run-android
+```
