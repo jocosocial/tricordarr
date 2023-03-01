aefa72910bdd61e78853b49522b2780e1442cc7a
Dustin Hendrickson <1521835+hendricksond@users.noreply.github.com>
2023-02-28 21:18:10 -0500

    Fix back button/loading behavior on webview, hook up seamail button

--- src/components/Views/TwitarrView.js
+++ src/components/Views/TwitarrView.js
@@ -1,10 +1,11 @@
 import {WebView} from 'react-native-webview';
 import React, {useEffect, useState, useRef} from 'react';
 import {AppSettings} from '../../libraries/AppSettings';
-import {BackHandler} from 'react-native';
+import {BackHandler, ActivityIndicator} from 'react-native';
 
 export const TwitarrView = ({ route }) => {
   const [url, setUrl] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const webViewRef = useRef();
 
   const handleBackButtonPress = () => {
@@ -16,30 +17,40 @@ export const TwitarrView = ({ route }) => {
     }
   }
 
-  useEffect(() => {  
-    BackHandler.addEventListener('hardwareBackPress', handleBackButtonPress);
+  const handleWebViewNavigationStateChange = (newNavState) => {
+    const { canGoBack } = newNavState;
+    if (canGoBack) {
+      BackHandler.addEventListener('hardwareBackPress', handleBackButtonPress);
+    }
+    else {
+      BackHandler.removeEventListener("hardwareBackPress", handleBackButtonPress);
+    }
+  }
 
+  useEffect(() => {
     const loadSettings = async () => {
       let url = await AppSettings.SERVER_URL.getValue();
-
-      console.log("ROUTE: ", route);
 
       if(route?.params?.resource) {
         url += `/${route.params.resource}`;
 
-        if(route?.params?.id) {
+        if(route.params.id) {
           url += `/${route.params.id}`;
         }
       }
-      
+
       setUrl(url);
-    };
+      setIsLoading(false);
+    }
+
     loadSettings();
+  }, [route?.params?.resource, route?.params?.id, isLoading]);
 
-    return () => {
-      BackHandler.removeEventListener("hardwareBackPress", handleBackButtonPress);
-    };
-  }, [route?.params?.resource, route?.params?.id]);
-
-  return <WebView source={{ uri: url }} ref={webViewRef} />
+  return (
+    isLoading ? <ActivityIndicator /> :
+    <WebView source={{ uri: url }}
+      ref={webViewRef} 
+      onNavigationStateChange={handleWebViewNavigationStateChange}
+    />
+  );
 };
