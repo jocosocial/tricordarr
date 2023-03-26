6455e25af7e1fa91cb2ed5e0769af5d432edc9e6
Grant Cohoe <grant@grantcohoe.com>
2023-03-26 17:07:51 -0400

    add seamail time badge

--- src/components/Lists/Seamail/SeamailListItem.tsx
+++ src/components/Lists/Seamail/SeamailListItem.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import {Badge, List} from 'react-native-paper';
+import {List} from 'react-native-paper';
 import {useUserData} from '../../Context/Contexts/UserDataContext';
 import {FezAvatarImage} from '../../Images/FezAvatarImage';
 import {commonStyles} from '../../../styles';
 import {FezDataProps} from '../../../libraries/Types';
-import {RelativeTimeTag} from '../../Text/RelativeTimeTag';
-import {View} from 'react-native';
-
-const SeamailRight = ({date, badgeCount}: {date: Date; badgeCount: number}) => {
-  return (
-    <View style={commonStyles.verticalCenterContainer}>
-      <View style={commonStyles.flexRow}>
-        <RelativeTimeTag date={date} />
-        {!!badgeCount && <Badge style={commonStyles.marginLeftSmall}>{badgeCount}</Badge>}
-      </View>
-    </View>
-  );
-};
+import {SeamailTimeBadge} from './SeamailTimeBadge';
 
 export const SeamailListItem = ({fez}: FezDataProps) => {
   const {profilePublicData} = useUserData();
@@ -39,6 +27,7 @@ export const SeamailListItem = ({fez}: FezDataProps) => {
 
   const getAvatar = () => <FezAvatarImage fez={fez} />;
   const onPress = () => console.log('foo');
+  const getTimeBadge = () => <SeamailTimeBadge date={fez.lastModificationTime} badgeCount={badgeCount} />;
 
   return (
     <List.Item
@@ -49,7 +38,7 @@ export const SeamailListItem = ({fez}: FezDataProps) => {
       descriptionStyle={styles.description}
       onPress={onPress}
       left={getAvatar}
-      right={() => <SeamailRight date={fez.lastModificationTime} badgeCount={badgeCount} />}
+      right={getTimeBadge}
     />
   );
 };
