68ce934cc9a9e5fa2e5c2a60bb2cc287e3fa8289
Grant Cohoe <grant@grantcohoe.com>
2023-03-26 22:59:33 -0400

    new fab

--- src/components/Buttons/FloatingActionButtons/SeamailNewFAB.tsx
+++ src/components/Buttons/FloatingActionButtons/SeamailNewFAB.tsx
@@ -1,16 +1,66 @@
 import * as React from 'react';
-import {StyleSheet} from 'react-native';
-import {FAB} from 'react-native-paper';
-
-const styles = StyleSheet.create({
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  },
-});
+import {FAB, Portal, Provider} from 'react-native-paper';
+import {useAppTheme} from '../../../styles/Theme';
+import {styleDefaults} from '../../../styles';
 
 export const SeamailNewFAB = () => {
-  return <FAB icon="plus" style={styles.fab} onPress={() => console.log('Pressed')} />;
+  const [state, setState] = React.useState({open: false});
+  const theme = useAppTheme();
+
+  const onStateChange = ({open}: {open: boolean}) => setState({open});
+
+  const {open} = state;
+
+  return (
+    <Provider>
+      <Portal>
+        <FAB.Group
+          open={open}
+          visible
+          icon={open ? 'chat' : 'plus'}
+          theme={theme}
+          variant={'primary'}
+          color={theme.colors.onPrimary}
+          fabStyle={{
+            backgroundColor: theme.colors.primary,
+          }}
+          actions={[
+            {
+              icon: 'email',
+              label: 'New Seamail',
+              color: theme.colors.onPrimary,
+              style: {
+                backgroundColor: theme.colors.primary,
+              },
+              labelStyle: {
+                backgroundColor: theme.colors.surfaceVariant,
+                color: theme.colors.onSurfaceVariant,
+                paddingVertical: styleDefaults.marginSize / 4,
+                paddingHorizontal: styleDefaults.marginSize / 2,
+                borderRadius: theme.roundness,
+              },
+              onPress: () => console.log('New Seamail'),
+            },
+            {
+              icon: 'phone-outgoing',
+              label: 'KrakenTalk',
+              color: theme.colors.onPrimary,
+              style: {
+                backgroundColor: theme.colors.primary,
+              },
+              labelStyle: {
+                backgroundColor: theme.colors.surfaceVariant,
+                color: theme.colors.onSurfaceVariant,
+                paddingVertical: styleDefaults.marginSize / 4,
+                paddingHorizontal: styleDefaults.marginSize / 2,
+                borderRadius: theme.roundness,
+              },
+              onPress: () => console.log('Starting KrakenTalk'),
+            },
+          ]}
+          onStateChange={onStateChange}
+        />
+      </Portal>
+    </Provider>
+  );
 };
