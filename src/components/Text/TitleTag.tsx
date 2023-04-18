de417c215b3c40cda62430adba285b9d840db280
Grant Cohoe <grant@grantcohoe.com>
2023-04-17 23:39:12 -0400

    cleanup

--- src/components/Text/TitleTag.tsx
+++ src/components/Text/TitleTag.tsx
@@ -2,11 +2,11 @@ import React, {PropsWithChildren} from 'react';
 import {Text} from 'react-native-paper';
 import {MD3TypescaleKey} from 'react-native-paper/lib/typescript/types';
 import {useStyles} from '../Context/Contexts/StyleContext';
-import {TextStyle} from 'react-native';
+import {StyleProp, TextStyle} from 'react-native';
 
 interface TitleTagProps {
   variant?: keyof typeof MD3TypescaleKey;
-  style?: TextStyle[];
+  style?: StyleProp<TextStyle>;
 }
 export const TitleTag = ({children, variant = 'titleSmall', style = undefined}: PropsWithChildren<TitleTagProps>) => {
   const {commonStyles} = useStyles();
