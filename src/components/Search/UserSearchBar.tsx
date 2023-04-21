9541e637d3415dd4a26659d5bab15b75738d364f
Grant Cohoe <grant@grantcohoe.com>
2023-04-21 17:21:41 -0400

    bring back the chips field

--- src/components/Search/UserSearchBar.tsx
+++ src/components/Search/UserSearchBar.tsx
@@ -7,26 +7,20 @@ import {ListSection} from '../Lists/ListSection';
 import {UserListItem} from '../Lists/Items/UserListItem';
 
 interface UserSearchBarProps {
-  participants: UserHeader[];
-  setParticipants: React.Dispatch<React.SetStateAction<UserHeader[]>>;
+  userHeaders: UserHeader[];
+  onPress: (user: UserHeader) => void;
 }
 
-export const UserSearchBar = ({participants, setParticipants}: UserSearchBarProps) => {
+export const UserSearchBar = ({userHeaders, onPress}: UserSearchBarProps) => {
   const [searchQuery, setSearchQuery] = React.useState('');
-  let {data} = useQuery<UserHeader[]>({
+  const {data} = useQuery<UserHeader[]>({
     queryKey: [`/users/match/allnames/${searchQuery}`],
     enabled: searchQuery.length >= 2,
   });
 
   const onChangeSearch = (query: string) => setSearchQuery(query);
 
-  const addParticipant = (user: UserHeader) => {
-    // https://stackoverflow.com/questions/1988349/array-push-if-does-not-exist
-    const existingIndex = participants.findIndex(item => item.userID === user.userID);
-    if (existingIndex === -1) {
-      setParticipants(participants.concat([user]));
-    }
-  };
+  const handlePress = (user: UserHeader) => onPress(user);
 
   // https://stackoverflow.com/questions/8668174/indexof-method-in-an-object-array
   return (
@@ -35,10 +29,10 @@ export const UserSearchBar = ({participants, setParticipants}: UserSearchBarProp
       <ListSection>
         {data &&
           data
-            // data (the search results) gets filtered to remove participants
-            // that are already present (participants.indexOf "exists")
-            .filter(user => participants.map(p => p.userID).indexOf(user.userID) === -1)
-            .flatMap(user => <UserListItem key={user.userID} userHeader={user} onPress={() => addParticipant(user)} />)}
+            // data (the search results) gets filtered to remove headers
+            // that are already present (userHeaders.indexOf "exists")
+            .filter(user => userHeaders.map(p => p.userID).indexOf(user.userID) === -1)
+            .flatMap(user => <UserListItem key={user.userID} userHeader={user} onPress={() => handlePress(user)} />)}
       </ListSection>
     </View>
   );
