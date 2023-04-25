dcf521358d50f8f9d59e68e881151bbdfc7b7e5d
Grant Cohoe <grant@grantcohoe.com>
2023-04-25 18:26:43 -0400

    button titles

--- src/components/Navigation/Stacks/SeamailStack.tsx
+++ src/components/Navigation/Stacks/SeamailStack.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {createNativeStackNavigator, NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {useTheme} from 'react-native-paper';
+import {useTheme, Text} from 'react-native-paper';
 import {SeamailListScreen} from '../../Screens/Seamail/SeamailListScreen';
 import {SeamailScreen} from '../../Screens/Seamail/SeamailScreen';
 import {SeamailStackScreenComponents} from '../../../libraries/Enums/Navigation';
 import {SeamailDetailsScreen} from '../../Screens/Seamail/SeamailDetailsScreen';
 import {UserProfileScreen} from '../../Screens/User/UserProfileScreen';
-import {useNavigation} from '@react-navigation/native';
+import {RouteProp, useNavigation} from '@react-navigation/native';
 import {SeamailCreateScreen} from '../../Screens/Seamail/SeamailCreateScreen';
 import {KrakenTalkCreateScreen} from '../../Screens/KrakenTalk/KrakenTalkCreateScreen';
+import {TextStyle, TouchableOpacity, View} from 'react-native';
+import {useStyles} from '../../Context/Contexts/StyleContext';
+import {NavHeaderTitle} from '../../Text/NavHeaderTitle';
+import {FezDataProps} from '../../../libraries/Types';
+import {SeamailHeaderTitle} from '../Components/SeamailHeaderTitle';
 
 // Beware: https://github.com/react-navigation/react-navigation/issues/10802
 export type SeamailStackParamList = {
@@ -27,6 +32,12 @@ export type SeamailStackParamList = {
   SeamailCreateScreen: undefined;
   KrakenTalkCreateScreen: undefined;
 };
+
+// const getSeamailoptions = (route: RouteProp<SeamailStackParamList, 'SeamailScreen'>) => ({
+//   headerTitle: () => <SeamailHeaderTitle fezID={route.params.fezID} title={route.params.title} />,
+// });
+
+const getHeader = (fezID: string, title: string) => <SeamailHeaderTitle fezID={fezID} title={title} />;
 
 export const SeamailStack = () => {
   const Stack = createNativeStackNavigator<SeamailStackParamList>();
@@ -51,7 +62,12 @@ export const SeamailStack = () => {
       <Stack.Screen
         name={SeamailStackScreenComponents.seamailScreen}
         component={SeamailScreen}
-        options={({route}) => ({title: route.params.title})}
+        // options={getSeamailoptions}
+        options={({route}) => ({
+          headerTitle: () => <SeamailHeaderTitle fezID={route.params.fezID} title={route.params.title} />,
+        })}
+        // options={({route}) => ({headerTitle: (route.params.fezID, route.params.title) => getHeaderTitle})}
+        // options={({route}) => ({title: route.params.title})}
       />
       <Stack.Screen
         name={SeamailStackScreenComponents.seamailDetailsScreen}
