7f51821f58542956bf770d0c3df7f11edc7450a5
Grant Cohoe <grant@grantcohoe.com>
2023-04-25 19:28:42 -0400

    icon cleanup

--- src/libraries/Enums/Icons.ts
+++ src/libraries/Enums/Icons.ts
@@ -3,6 +3,8 @@ export enum AppIcons {
   seamailCreate = 'email-plus',
   krakentalkCreate = 'phone-outgoing',
   chat = 'chat',
+  // Profile
+  privateNoteEdit = 'note-edit',
   // Generic
   new = 'plus',
   newImage = 'image-plus',
@@ -11,6 +13,10 @@ export enum AppIcons {
   reload = 'reload',
   error = 'alert-circle-outline',
   menu = 'dots-vertical',
+  favorite = 'star',
+  block = 'cancel',
+  mute = 'volume-off',
+  report = 'alert-octagon',
   // Users
   user = 'account',
   group = 'account-group',
