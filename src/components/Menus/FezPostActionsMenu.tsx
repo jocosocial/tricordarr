2fd90a8c455be484d41851a699b88bb9170a5382
Grant Cohoe <grant@grantcohoe.com>
2023-05-02 17:11:49 -0400

    improve menu structure

--- src/components/Menus/FezPostActionsMenu.tsx
+++ src/components/Menus/FezPostActionsMenu.tsx
@@ -1,21 +1,28 @@
-import React, {PropsWithChildren, ReactNode} from 'react';
+import React, {ReactNode} from 'react';
 import {Menu} from 'react-native-paper';
+import {AppIcons} from '../../libraries/Enums/Icons';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {FezPostData} from '../../libraries/Structs/ControllerStructs';
 
 interface FezPostActionsMenuProps {
   visible: boolean;
   closeMenu: () => void;
   anchor: ReactNode;
+  fezPost: FezPostData;
 }
 
-export const FezPostActionsMenu = ({
-  visible,
-  closeMenu,
-  anchor,
-  children,
-}: PropsWithChildren<FezPostActionsMenuProps>) => {
+export const FezPostActionsMenu = ({visible, closeMenu, anchor, fezPost}: FezPostActionsMenuProps) => {
   return (
     <Menu visible={visible} onDismiss={closeMenu} anchor={anchor}>
-      {children}
+      <Menu.Item
+        dense={true}
+        leadingIcon={AppIcons.copy}
+        title={'Copy'}
+        onPress={() => {
+          Clipboard.setString(fezPost.text);
+          closeMenu();
+        }}
+      />
     </Menu>
   );
 };
--- src/components/Views/MessageView.tsx
+++ src/components/Views/MessageView.tsx
@@ -15,21 +15,20 @@ import {RelativeTimeTag} from '../Text/RelativeTimeTag';
 import {FezPostActionsMenu} from '../Menus/FezPostActionsMenu';
 import {AppIcons} from '../../libraries/Enums/Icons';
 import Clipboard from '@react-native-clipboard/clipboard';
+import {FezPostData} from '../../libraries/Structs/ControllerStructs';
 
 interface MessageViewProps {
-  text: string;
-  author?: string;
+  fezPost: FezPostData;
   postBySelf?: boolean;
-  timestamp?: Date;
+  showAuthor?: boolean;
 }
 
 /**
  * This is a View container for a text message in the style of Android Messages or Signal.
  * It only contains the message itself.
  */
-export const MessageView = ({text, author, timestamp = undefined, postBySelf = false}: MessageViewProps) => {
+export const MessageView = ({fezPost, postBySelf = false, showAuthor}: MessageViewProps) => {
   const {commonStyles} = useStyles();
-  const messageRef = useRef<View>(null);
   const [menuVisible, setMenuVisible] = useState(false);
   const openMenu = () => setMenuVisible(true);
   const closeMenu = () => setMenuVisible(false);
@@ -54,22 +53,16 @@ export const MessageView = ({text, author, timestamp = undefined, postBySelf = f
   return (
     <View style={styles.messageView}>
       <TouchableOpacity style={styles.opacity} onLongPress={openMenu}>
-        {!!author && <Text style={styles.messageTextHeader}>{author}</Text>}
+        {showAuthor && <Text style={styles.messageTextHeader}>{fezPost.author.username}</Text>}
         <FezPostActionsMenu
           visible={menuVisible}
           closeMenu={closeMenu}
-          anchor={<Text style={styles.messageText}>{text}</Text>}>
-          <Menu.Item
-            dense={true}
-            leadingIcon={AppIcons.copy}
-            title={'Copy'}
-            onPress={() => {
-              Clipboard.setString(text);
-              closeMenu();
-            }}
-          />
-        </FezPostActionsMenu>
-        {timestamp && <RelativeTimeTag date={timestamp} style={styles.messageDateText} variant={'labelSmall'} />}
+          anchor={<Text style={styles.messageText}>{fezPost.text}</Text>}
+          fezPost={fezPost}
+        />
+        {fezPost.timestamp && (
+          <RelativeTimeTag date={fezPost.timestamp} style={styles.messageDateText} variant={'labelSmall'} />
+        )}
       </TouchableOpacity>
     </View>
   );
