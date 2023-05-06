c293d704ef2bebd43ab67fe132c3472fbb932f14
Grant Cohoe <grant@grantcohoe.com>
2023-05-06 18:17:08 -0400

    author and orientation for privileged convos

--- src/components/Lists/Items/FezPostListItem.tsx
+++ src/components/Lists/Items/FezPostListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FezPostData} from '../../../libraries/Structs/ControllerStructs';
+import {FezData, FezPostData} from '../../../libraries/Structs/ControllerStructs';
 import {useUserData} from '../../Context/Contexts/UserDataContext';
 import {UserAvatarImage} from '../../Images/UserAvatarImage';
 import {MessageView} from '../../Views/MessageView';
@@ -9,43 +9,59 @@ import {MessageAvatarContainerView} from '../../Views/MessageAvatarContainerView
 import {FlatListItemContent} from '../../Views/Content/FlatListItemContent';
 import {SeamailStackScreenComponents} from '../../../libraries/Enums/Navigation';
 import {useSeamailStack} from '../../Navigation/Stacks/SeamailStack';
+import {usePrivilege} from '../../Context/Contexts/PrivilegeContext';
 
 // https://github.com/akveo/react-native-ui-kitten/issues/1167
 interface FezPostListItemProps {
-  item: FezPostData;
+  fez: FezData;
+  fezPost: FezPostData;
   index: number;
   separators: {
     highlight: () => void;
     unhighlight: () => void;
     updateProps: (select: 'leading' | 'trailing', newProps: any) => void;
   };
-  showAuthor: boolean;
 }
 
-export const FezPostListItem = ({item, index, separators, showAuthor = true}: FezPostListItemProps) => {
+export const FezPostListItem = ({fezPost, index, separators, fez}: FezPostListItemProps) => {
   const {profilePublicData} = useUserData();
-  const postBySelf = profilePublicData.header.userID === item.author.userID;
   const navigation = useSeamailStack();
+  const {asPrivilegedUser} = usePrivilege();
+
+  let showAuthor = fez.participantCount > 2;
+
+  // Do not show the author for the users own messages.
+  if (fezPost.author.userID === profilePublicData.header.userID) {
+    showAuthor = false;
+  }
+
+  // Always show the author of all messages in privileged conversations.
+  if (asPrivilegedUser) {
+    showAuthor = true;
+  }
+
+  const messageOnRight =
+    fezPost.author.userID === profilePublicData.header.userID || fezPost.author.username === asPrivilegedUser;
 
   const onPress = () => {
     navigation.push(SeamailStackScreenComponents.userProfileScreen, {
-      userID: item.author.userID,
-      username: item.author.username,
+      userID: fezPost.author.userID,
+      username: fezPost.author.username,
     });
   };
 
   return (
     <FlatListItemContent>
-      {!postBySelf && (
+      {!messageOnRight && (
         <MessageAvatarContainerView onPress={onPress}>
-          <UserAvatarImage userID={item.author.userID} small={true} />
+          <UserAvatarImage userID={fezPost.author.userID} small={true} />
         </MessageAvatarContainerView>
       )}
-      {postBySelf && <MessageSpacerView />}
+      {messageOnRight && <MessageSpacerView />}
       <MessageViewContainer>
-        <MessageView fezPost={item} postBySelf={postBySelf} showAuthor={showAuthor} />
+        <MessageView fezPost={fezPost} messageOnRight={messageOnRight} showAuthor={showAuthor} />
       </MessageViewContainer>
-      {!postBySelf && <MessageSpacerView />}
+      {!messageOnRight && <MessageSpacerView />}
     </FlatListItemContent>
   );
 };
