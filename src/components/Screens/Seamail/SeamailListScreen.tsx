8313a9387c54df26b671d134df73edad0a7c9b14
Grant Cohoe <grant@grantcohoe.com>
2023-05-06 21:24:16 -0400

    fezlist provider

--- src/components/Screens/Seamail/SeamailListScreen.tsx
+++ src/components/Screens/Seamail/SeamailListScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {RefreshControl, View} from 'react-native';
 import {Divider} from 'react-native-paper';
 import {ScrollingContentView} from '../../Views/Content/ScrollingContentView';
@@ -14,14 +14,19 @@ import {SeamailNewFAB} from '../../Buttons/FloatingActionButtons/SeamailNewFAB';
 import {ListSection} from '../../Lists/ListSection';
 import {useSeamailQuery} from '../../Queries/Fez/FezQueries';
 import {usePrivilege} from '../../Context/Contexts/PrivilegeContext';
+import {useTwitarr} from '../../Context/Contexts/TwitarrContext';
 
 export const SeamailListScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
   const {isLoggedIn, isLoading, isPrivileged} = useUserData();
-  const {asPrivilegedUser, clearPrivileges} = usePrivilege();
-  // const {setUserNotificationData} = useUserNotificationData();
-  console.log('asPrivilegedUser', asPrivilegedUser);
+  const {asPrivilegedUser} = usePrivilege();
   const {data, refetch} = useSeamailQuery(asPrivilegedUser);
+  const {fezList, setFezList} = useTwitarr();
+
+  useEffect(() => {
+    setFezList(data);
+    return () => setFezList(undefined);
+  }, [data, setFezList]);
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
@@ -50,8 +55,8 @@ export const SeamailListScreen = () => {
           )}
           <ListSection>
             <Divider bold={true} />
-            {data &&
-              data.fezzes.map(fez => (
+            {fezList &&
+              fezList.fezzes.map(fez => (
                 <View key={fez.fezID}>
                   <SeamailListItem fez={fez} />
                   <Divider bold={true} />
