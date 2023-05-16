f24c5ff78aec20fda579862f4658b073e71c2231
Grant Cohoe <grant@grantcohoe.com>
2023-05-15 20:53:58 -0400

    switch to new fezlist structure

--- src/components/Context/Contexts/TwitarrContext.ts
+++ src/components/Context/Contexts/TwitarrContext.ts
@@ -1,17 +1,15 @@
 import {useContext, createContext, Dispatch, SetStateAction} from 'react';
-import {FezData, FezListData, FezPostData} from '../../../libraries/Structs/ControllerStructs';
+import {FezData, FezPostData} from '../../../libraries/Structs/ControllerStructs';
 import {FezListActionsType} from '../../Reducers/FezListReducers';
 import {FezPostsActionsType} from '../../Reducers/FezPostsReducers';
 
 interface TwitarrContextType {
   fez?: FezData;
   setFez: Dispatch<SetStateAction<FezData | undefined>>;
-  fezList?: FezListData;
+  fezList: FezData[];
   dispatchFezList: Dispatch<FezListActionsType>;
   fezPostsData: FezPostData[];
   dispatchFezPostsData: Dispatch<FezPostsActionsType>;
-  newFezList: FezData[];
-  setNewFezList: Dispatch<SetStateAction<FezData[]>>;
 }
 
 export const TwitarrContext = createContext(<TwitarrContextType>{});
