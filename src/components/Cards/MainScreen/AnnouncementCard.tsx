5f69ad90b66c989556bbc1e45c475a16997c3320
Grant Cohoe <grant@grantcohoe.com>
2023-07-16 00:02:43 -0400

    cleanup styles

--- src/components/Cards/MainScreen/AnnouncementCard.tsx
+++ src/components/Cards/MainScreen/AnnouncementCard.tsx
@@ -1,24 +1,21 @@
 import {Card, Text} from 'react-native-paper';
-import {AndroidColor} from '@notifee/react-native';
 import React from 'react';
-import {useAppTheme} from '../../../styles/Theme';
 import {AnnouncementData} from '../../../libraries/Structs/ControllerStructs';
 import {useStyles} from '../../Context/Contexts/StyleContext';
 
 export const AnnouncementCard = ({announcement}: {announcement: AnnouncementData}) => {
-  const theme = useAppTheme();
   const {commonStyles} = useStyles();
 
   return (
-    <Card style={[commonStyles.marginBottomSmall, {backgroundColor: theme.colors.twitarrPositiveButton}]}>
+    <Card style={[commonStyles.marginBottomSmall, commonStyles.twitarrPositive]}>
       <Card.Title
         title={`From @${announcement.author.username}:`}
-        titleStyle={{color: AndroidColor.WHITE, fontWeight: 'bold'}}
+        titleStyle={[commonStyles.onTwitarrButton, commonStyles.bold]}
         subtitleVariant={'bodyLarge'}
-        subtitleStyle={{color: AndroidColor.WHITE}}
+        subtitleStyle={[commonStyles.onTwitarrButton]}
       />
       <Card.Content>
-        <Text style={{color: AndroidColor.WHITE}}>{announcement.text}</Text>
+        <Text style={[commonStyles.onTwitarrButton]}>{announcement.text}</Text>
       </Card.Content>
     </Card>
   );
--- src/components/Cards/MainScreen/DailyThemeCard.tsx
+++ src/components/Cards/MainScreen/DailyThemeCard.tsx
@@ -1,14 +1,15 @@
 import {Card, Text} from 'react-native-paper';
-import {AndroidColor} from '@notifee/react-native';
 import React, {useEffect, useState} from 'react';
-import {useAppTheme} from '../../../styles/Theme';
 import {useStyles} from '../../Context/Contexts/StyleContext';
 import {useDailyThemeQuery} from '../../Queries/Alert/DailyThemeQueries';
 import {useCruise} from '../../Context/Contexts/CruiseContext';
 import {DailyThemeData} from '../../../libraries/Structs/ControllerStructs';
 
+/**
+ * A card to display the daily theme object as returned from the API. If no object then no theme.
+ * The site UI invents some themes for days that don't have them.
+ */
 export const DailyThemeCard = () => {
-  const theme = useAppTheme();
   const {data: dailyThemeData} = useDailyThemeQuery();
   const {cruiseDayIndex} = useCruise();
   const [dailyTheme, setDailyTheme] = useState<DailyThemeData>();
@@ -36,16 +37,16 @@ export const DailyThemeCard = () => {
   }
 
   return (
-    <Card style={[commonStyles.marginBottomSmall, {backgroundColor: theme.colors.twitarrNeutralButton}]}>
+    <Card style={[commonStyles.marginBottomSmall, commonStyles.twitarrNeutral]}>
       <Card.Title
         title={"Today's Theme:"}
         subtitle={dailyTheme.title}
-        titleStyle={{color: AndroidColor.WHITE, fontWeight: 'bold'}}
+        titleStyle={[commonStyles.onTwitarrButton, commonStyles.bold]}
         subtitleVariant={'bodyLarge'}
-        subtitleStyle={{color: AndroidColor.WHITE}}
+        subtitleStyle={[commonStyles.onTwitarrButton]}
       />
       <Card.Content>
-        <Text style={{color: AndroidColor.WHITE}}>{dailyTheme.info}</Text>
+        <Text style={[commonStyles.onTwitarrButton]}>{dailyTheme.info}</Text>
       </Card.Content>
     </Card>
   );
