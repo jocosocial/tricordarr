6b4adf8909ff127ac7ac6938f3a0fac9ae3c88dc
Grant Cohoe <grant@grantcohoe.com>
2023-07-15 22:18:28 -0400

    announcement stuff

--- src/components/Views/Static/MainView.tsx
+++ src/components/Views/Static/MainView.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import {Card, Text} from 'react-native-paper';
 import {AppView} from '../AppView';
 import {ScrollingContentView} from '../Content/ScrollingContentView';
 import {PaddedContentView} from '../Content/PaddedContentView';
@@ -7,12 +6,6 @@ import {MainStackComponents, NavigatorIDs} from '../../../libraries/Enums/Naviga
 import {MainStackParamList} from '../../Navigation/Stacks/MainStack';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useDrawer} from '../../Context/Contexts/DrawerContext';
-import MainViewDayImage from '../../../../assets/mainview_day.jpg';
-import {useConfig} from '../../Context/Contexts/ConfigContext';
-import {useStyles} from '../../Context/Contexts/StyleContext';
-import {useAppTheme} from '../../../styles/Theme';
-import {AndroidColor} from '@notifee/react-native';
-import {useDailyThemeQuery} from '../../Queries/Alert/DailyThemeQueries';
 import {DailyThemeCard} from '../../Cards/MainScreen/DailyThemeCard';
 import {HeaderCard} from '../../Cards/MainScreen/HeaderCard';
 import {MainAnnouncementView} from '../MainAnnouncementView';
@@ -21,9 +14,6 @@ type Props = NativeStackScreenProps<MainStackParamList, MainStackComponents.main
 
 export const MainView = ({navigation}: Props) => {
   const {getLeftMainHeaderButtons} = useDrawer();
-  const {appConfig} = useConfig();
-  const {commonStyles} = useStyles();
-  const theme = useAppTheme();
 
   useEffect(() => {
     navigation.setOptions({
@@ -35,11 +25,6 @@ export const MainView = ({navigation}: Props) => {
     <AppView>
       <ScrollingContentView>
         <PaddedContentView>
-          {/*<Text variant={'displayMedium'}>Hello Boat!</Text>*/}
-          {/*<Text variant={'titleLarge'} style={commonStyles.marginTop}>*/}
-          {/*  ⚠️ Warning ⚠️*/}
-          {/*</Text>*/}
-          {/*<Text>This app is an extremely experimental prototype and should be treated as such.</Text>*/}
           <HeaderCard />
           <DailyThemeCard />
           <MainAnnouncementView />
