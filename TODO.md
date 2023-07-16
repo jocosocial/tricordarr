1c8dc879553a37a75a28d3c3b5c5d47ca4741bc1
Grant Cohoe <grant@grantcohoe.com>
2023-07-15 23:56:01 -0400

    add image card

--- TODO.md
+++ TODO.md
@@ -4,3 +4,4 @@ Work Queue
 * Figure out how to go back to an arbitrary screen
 * Try to make a generic form?
 * API is not decoding dates correctly
+* timezone warnings
--- src/components/Cards/MainScreen/MainImageCardCover.tsx
+++ src/components/Cards/MainScreen/MainImageCardCover.tsx
@@ -6,18 +6,36 @@ import DayImage from '../../../../assets/mainview_day.jpg';
 import NightImage from '../../../../assets/mainview_night.jpg';
 // @ts-ignore
 import SunsetImage from '../../../../assets/mainview_sunset.jpg';
+// @ts-ignore
+import HappyHourImage from '../../../../assets/mainview_happy.jpg';
+import {useUserNotificationData} from '../../Context/Contexts/UserNotificationDataContext';
 
+/**
+ * Display a pretty image in the app based on the time of day.
+ */
 export const MainImageCardCover = () => {
+  const {userNotificationData} = useUserNotificationData();
 
-  // @TODO factor in server time offset.
-  const currentHour = new Date().getHours();
+  // Default to local, but override with the server offset.
+  let currentHour = new Date().getHours();
+  if (userNotificationData) {
+    // Take the timestamp that the server gives us (should be UTC), then apply the offset in milliseconds.
+    // This becomes a new relative date that should match what the user is really experiencing.
+    const serverTimestamp = Date.parse(userNotificationData.serverTime);
+    const relativeTimestamp = serverTimestamp + userNotificationData.serverTimeOffset * 1000;
+    const relativeDate = new Date(relativeTimestamp);
+    currentHour = relativeDate.getUTCHours();
+  }
 
   // 8PM-5AM Night
-  // 6AM-4PM Day
+  // 6AM-3PM Day
+  // 4PM Happy Hour
   // 5PM-7PM Sunset
   let sourceImage = NightImage;
-  if (currentHour >= 6 && currentHour <= 16) {
+  if (currentHour >= 6 && currentHour <= 15) {
     sourceImage = DayImage;
+  } else if (currentHour === 16) {
+    sourceImage = HappyHourImage;
   } else if (currentHour >= 17 && currentHour <= 19) {
     sourceImage = SunsetImage;
   }
