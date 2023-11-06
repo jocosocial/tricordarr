95e1f0b8063f31042de18784a375d264ea25752c
Grant Cohoe <grant@grantcohoe.com>
2023-11-06 00:35:11 +0000

    start adding lfgcard

---
+++ src/components/Cards/Schedule/LfgCard.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {Card} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {parseISO} from 'date-fns';
+import {useStyles} from '../../Context/Contexts/StyleContext';
+import {useAppTheme} from '../../../styles/Theme';
+import {useCruise} from '../../Context/Contexts/CruiseContext';
+import useDateTime, {calcCruiseDayTime, getTimeZoneOffset} from '../../../libraries/DateTime';
+import {ScheduleItem} from '../../../libraries/Types';
+import {EventCardNowView} from '../../Views/Schedule/EventCardNowView';
+import {EventCardSoonView} from '../../Views/Schedule/EventCardSoonView';
+import {EventCardBody} from '../../Views/Schedule/EventCardBody';
+import {FezData} from "../../../libraries/Structs/ControllerStructs";
+
+interface LfgCardProps {
+  fez: FezData;
+}
+
+export const LfgCard = ({fez}: LfgCardProps) => {
+  const {commonStyles} = useStyles();
+  const theme = useAppTheme();
+  const {startDate, endDate} = useCruise();
+  const minutelyUpdatingDate = useDateTime('minute');
+
+  const styles = StyleSheet.create({
+    cardTitle: {
+      ...commonStyles.paddingLeftZero,
+    },
+    contentView: {
+      ...commonStyles.flexRow,
+      ...commonStyles.alignItemsCenter,
+    },
+    cardContent: {
+      ...commonStyles.paddingVerticalZero,
+      ...commonStyles.paddingLeftZero,
+      ...commonStyles.justifyCenter,
+    },
+    officialCard: {
+      backgroundColor: theme.colors.twitarrNeutralButton,
+    },
+    shadowCard: {
+      backgroundColor: theme.colors.jocoPurple,
+    },
+    lfgCard: {
+      backgroundColor: theme.colors.outline,
+    },
+  });
+
+  const itemStartTime = parseISO(item.startTime);
+  const itemEndTime = parseISO(item.endTime);
+  const eventStartDayTime = calcCruiseDayTime(itemStartTime, startDate, endDate);
+  const eventEndDayTime = calcCruiseDayTime(itemEndTime, startDate, endDate);
+  const nowDayTime = calcCruiseDayTime(minutelyUpdatingDate, startDate, endDate);
+  const tzOffset = getTimeZoneOffset('America/New_York', item.timeZone, item.startTime);
+
+  const cardStyle = {
+    ...(item.itemType === 'official' ? styles.officialCard : undefined),
+    ...(item.itemType === 'shadow' ? styles.shadowCard : undefined),
+    ...(item.itemType === 'lfg' ? styles.lfgCard : undefined),
+  };
+
+  return (
+    <Card mode={'contained'} style={cardStyle}>
+      <Card.Content style={styles.cardContent}>
+        <View style={styles.contentView}>
+          {nowDayTime.cruiseDay === eventStartDayTime.cruiseDay &&
+            nowDayTime.dayMinutes - tzOffset >= eventStartDayTime.dayMinutes &&
+            nowDayTime.dayMinutes - tzOffset < eventEndDayTime.dayMinutes && <EventCardNowView />}
+          {nowDayTime.cruiseDay === eventStartDayTime.cruiseDay &&
+            nowDayTime.dayMinutes - tzOffset >= eventStartDayTime.dayMinutes - 30 &&
+            nowDayTime.dayMinutes - tzOffset < eventStartDayTime.dayMinutes && <EventCardSoonView />}
+          <EventCardBody scheduleItem={item} includeDay={includeDay} />
+        </View>
+      </Card.Content>
+    </Card>
+  );
+};
