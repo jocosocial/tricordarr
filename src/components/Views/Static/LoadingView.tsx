97ef46d341c85803453586f55174e6495d85cf33
Grant Cohoe <grant@grantcohoe.com>
2023-11-20 00:13:25 -0500

    cleanup

--- src/components/Views/Static/LoadingView.tsx
+++ src/components/Views/Static/LoadingView.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import {ScrollView, View} from 'react-native';
+import {View} from 'react-native';
 import {ActivityIndicator, Text} from 'react-native-paper';
-import {AppView} from '../AppView';
 import {ScrollingContentView} from '../Content/ScrollingContentView';
 
 export const LoadingView = () => {
   return (
     <View>
       <ScrollingContentView>
-        <ScrollView>
-          <ActivityIndicator />
-          <Text>Loading...</Text>
-        </ScrollView>
+        <ActivityIndicator />
+        <Text>Loading...</Text>
       </ScrollingContentView>
     </View>
   );
