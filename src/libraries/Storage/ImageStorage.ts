769f3564698b8c60f7496dc50b1409c17f28ff5c
Grant Cohoe <grant@grantcohoe.com>
2023-11-20 22:57:14 -0500

    reduce permissions and improve mime processing

--- src/libraries/Storage/ImageStorage.ts
+++ src/libraries/Storage/ImageStorage.ts
@@ -1,27 +1,25 @@
 import {ImageQueryData} from '../Types';
-import {PERMISSIONS, request as requestPermission} from 'react-native-permissions';
 import RNFS from 'react-native-fs';
 import {CameraRoll} from '@react-native-camera-roll/camera-roll';
+import * as mime from 'react-native-mime-types';
 
 const extensionRegExp = new RegExp('\\.', 'i');
 
 const getImageDestinationPath = (fileName: string, mimeType: string) => {
   let destPath = `${RNFS.DocumentDirectoryPath}/${fileName}`;
-  if (!extensionRegExp.test(fileName) && mimeType === 'image/jpeg') {
-    destPath = `${destPath}.jpg`;
+  if (!extensionRegExp.test(fileName)) {
+    console.log('Adding MIME', mime.extension(mimeType));
+    destPath = `${destPath}.${mime.extension(mimeType)}`;
   }
   return destPath;
 };
 
 export const saveImageToLocal = async (imageData: ImageQueryData) => {
-  console.log('Saving image', imageData.fileName);
-  const permission = await requestPermission(PERMISSIONS.ANDROID.READ_MEDIA_IMAGES);
-  // check permission from here
   let destPath = getImageDestinationPath(imageData.fileName, imageData.mimeType);
   if (!imageData.base64) {
     throw Error(`No data to save to file ${destPath}`);
   }
-  console.log('Writing data to', destPath);
+  console.log('Writing data to', destPath, imageData.mimeType);
   await RNFS.writeFile(destPath, imageData.base64, 'base64');
   const cameraRollSaveResult = await CameraRoll.save(destPath, {
     type: 'photo',
