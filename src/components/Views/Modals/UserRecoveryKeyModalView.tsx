5c82114e883698a07a1f31a9f4ade127cbca5e0c
Grant Cohoe <grant@grantcohoe.com>
2024-02-02 10:43:10 -0500

    clarify long press recovery

--- src/components/Views/Modals/UserRecoveryKeyModalView.tsx
+++ src/components/Views/Modals/UserRecoveryKeyModalView.tsx
@@ -28,8 +28,8 @@ const UserRecoveryKeyModalContent = ({recoveryKey}: {recoveryKey: string}) => {
     <>
       <Text>
         Your account recovery key can be used if you lose access to your account and can't recover your password. It can
-        be used only once and will be shown to you only right now. Write it down (long press) or screenshot before
-        proceeding.
+        be used only once and will be shown to you only right now. Write it down (long press to copy to clipboard) or
+        screenshot this screen before proceeding.
       </Text>
       <Text style={keyStyle} variant={'titleLarge'} onLongPress={() => Clipboard.setString(recoveryKey)}>
         {recoveryKey}
