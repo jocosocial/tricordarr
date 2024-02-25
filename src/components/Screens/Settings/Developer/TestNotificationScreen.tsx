df8bae44e8aadfc990ed09d9d88d0bde7a74195e
Grant Cohoe <grant@grantcohoe.com>
2024-02-25 15:07:30 -0500

    fix issue with notification pressing

--- src/components/Screens/Settings/Developer/TestNotificationScreen.tsx
+++ src/components/Screens/Settings/Developer/TestNotificationScreen.tsx
@@ -6,6 +6,11 @@ import {ScrollingContentView} from '../../../Views/Content/ScrollingContentView'
 import {PaddedContentView} from '../../../Views/Content/PaddedContentView';
 import {AppView} from '../../../Views/AppView';
 import {useStyles} from '../../../Context/Contexts/StyleContext';
+import {generateContentNotification} from '../../../../libraries/Notifications/Content';
+import {eventChannel} from '../../../../libraries/Notifications/Channels';
+import {NotificationType} from '@notifee/react-native/dist/utils';
+import {NotificationTypeData} from '../../../../libraries/Structs/SocketStructs';
+import {PressAction} from '../../../../libraries/Enums/Notifications';
 
 export const TestNotificationScreen = () => {
   const theme = useAppTheme();
@@ -25,6 +30,22 @@ export const TestNotificationScreen = () => {
             buttonText="Cancel"
             buttonColor={theme.colors.twitarrNegativeButton}
             onPress={() => cancelTestNotification()}
+            style={[commonStyles.marginTopSmall]}
+          />
+          <PrimaryActionButton
+            buttonText={'Generate Event Notification'}
+            onPress={() =>
+              generateContentNotification(
+                'ABC123',
+                'Followed Event Starting',
+                'Orientation is starting Soon™ in World Stage, Deck 2, Forward',
+                eventChannel,
+                NotificationTypeData.followedEventStarting,
+                '/events/ABC123',
+                // '/forumpost/mentions',
+                PressAction.event,
+              )
+            }
             style={[commonStyles.marginTopSmall]}
           />
         </PaddedContentView>
