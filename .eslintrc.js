1ac6cbf2a36322524a1d83afb4ceea7aca7dc60b
Grant Cohoe <grant@grantcohoe.com>
2024-07-13 17:10:52 -0400

    Fix ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  // https://stackoverflow.com/questions/58065765/eslint-jest-globals-environment-key-unknown
+  extends: ['@react-native', 'plugin:jest/recommended'],
   rules: {
     'linebreak-style': 0,
   },
