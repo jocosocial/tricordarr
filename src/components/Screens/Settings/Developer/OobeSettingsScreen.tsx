1b1a6a24e46b40ab9074b683d24ccaeea5f810b1
Grant Cohoe <grant@grantcohoe.com>
2024-07-13 17:11:13 -0400

    Switch to socket reset rather than open/close

--- src/components/Screens/Settings/Developer/OobeSettingsScreen.tsx
+++ src/components/Screens/Settings/Developer/OobeSettingsScreen.tsx
@@ -1,19 +1,9 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import {AppView} from '../../../Views/AppView';
 import {ScrollingContentView} from '../../../Views/Content/ScrollingContentView';
 import {PaddedContentView} from '../../../Views/Content/PaddedContentView';
-import {SettingSwitch} from '../../../Switches/SettingSwitch';
 import {useConfig} from '../../../Context/Contexts/ConfigContext';
-import {DataTable, Divider, Text} from 'react-native-paper';
-import {SocketControlView} from '../../../Views/SocketControlView';
-import {RelativeTimeTag} from '../../../Text/Tags/RelativeTimeTag';
-import {SocketHealthcheckData} from '../../../../libraries/Structs/SocketStructs';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {StorageKeys} from '../../../../libraries/Storage';
-import {RefreshControl} from 'react-native';
-import {commonStyles} from '../../../../styles';
-import {useSocket} from '../../../Context/Contexts/SocketContext';
-import {WebSocketState} from '../../../../libraries/Network/Websockets';
+import {DataTable, Text} from 'react-native-paper';
 import {SettingDataTableRow} from '../../../DataTables/SettingDataTableRow';
 import {PrimaryActionButton} from '../../../Buttons/PrimaryActionButton';
 
