a111e3f471d7832764b0415ac72059e46d182e51
Grant Cohoe <grant@grantcohoe.com>
2024-07-14 12:21:54 -0400

    add photostream body

--- src/components/Lists/Items/PhotostreamListItem.tsx
+++ src/components/Lists/Items/PhotostreamListItem.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {PhotostreamImageData} from '../../../libraries/Structs/ControllerStructs.tsx';
 import {APIImage} from '../../Images/APIImage.tsx';
 import {View} from 'react-native';
-import {Text} from 'react-native-paper';
 import {PhotostreamImageHeaderView} from '../../Views/Photostream/PhotostreamImageHeaderView.tsx';
+import {PhotostreamImageBodyView} from '../../Views/Photostream/PhotostreamImageBodyView.tsx';
 
 interface PhotostreamListItemProps {
   item: PhotostreamImageData;
@@ -20,13 +20,7 @@ export const PhotostreamListItem = ({item}: PhotostreamListItemProps) => {
           fullPath={`/image/full/${item.image}`}
         />
       </View>
-      <View>
-        <Text>{item.location}</Text>
-
-      </View>
-      {/*<Card.Content>*/}
-      {/*  <Card.Title title={item.location} subtitle={} />*/}
-      {/*</Card.Content>*/}
+      <PhotostreamImageBodyView image={item} />
     </View>
   );
 };
