9fbe5c1d1bc04f672a00007eb0e2e7eb12114c13
Grant Cohoe <grant@grantcohoe.com>
2024-07-14 12:11:41 -0400

    more fun photostream

---
+++ src/components/Menus/Photostream/PhotostreamImageActionsMenu.tsx
@@ -0,0 +1,54 @@
+import React, {ReactNode} from 'react';
+import {Divider, Menu} from 'react-native-paper';
+import {AppIcons} from '../../../libraries/Enums/Icons';
+import {useModal} from '../../Context/Contexts/ModalContext';
+import {ReportModalView} from '../../Views/Modals/ReportModalView';
+import {PhotostreamImageData} from '../../../libraries/Structs/ControllerStructs.tsx';
+import {usePrivilege} from '../../Context/Contexts/PrivilegeContext.ts';
+import {ModerateMenuItem} from '../Items/ModerateMenuItem.tsx';
+import {useCommonStack} from '../../Navigation/CommonScreens.tsx';
+
+interface PhotostreamImageActionsMenuProps {
+  visible: boolean;
+  closeMenu: () => void;
+  anchor: ReactNode;
+  image: PhotostreamImageData;
+}
+
+export const PhotostreamImageActionsMenu = ({visible, closeMenu, anchor, image}: PhotostreamImageActionsMenuProps) => {
+  const {setModalContent, setModalVisible} = useModal();
+  const {hasModerator} = usePrivilege();
+  const commonNavigation = useCommonStack();
+
+  const handleReport = () => {
+    closeMenu();
+    setModalContent(<ReportModalView photostreamImage={image} />);
+    setModalVisible(true);
+  };
+
+  return (
+    <Menu visible={visible} onDismiss={closeMenu} anchor={anchor}>
+      <Menu.Item
+        dense={false}
+        leadingIcon={AppIcons.save}
+        title={'Share'}
+        onPress={() => {
+          // Clipboard.setString(fezPost.text);
+          closeMenu();
+        }}
+      />
+      <Menu.Item dense={false} leadingIcon={AppIcons.report} title={'Report'} onPress={handleReport} />
+      {hasModerator && (
+        <>
+          <Divider bold={true} />
+          <ModerateMenuItem
+            closeMenu={closeMenu}
+            resourceID={image.postID.toString()}
+            resource={'photostream'}
+            navigation={commonNavigation}
+          />
+        </>
+      )}
+    </Menu>
+  );
+};
