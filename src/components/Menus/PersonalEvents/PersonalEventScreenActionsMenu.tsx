16eba70447a1ee5fdfdec26999c1225582e2d63c
Grant Cohoe <grant@grantcohoe.com>
2024-07-28 23:09:40 -0400

    added the menu

--- src/components/Menus/PersonalEvents/PersonalEventScreenActionsMenu.tsx
+++ src/components/Menus/PersonalEvents/PersonalEventScreenActionsMenu.tsx
@@ -1,20 +1,18 @@
-import {EventData, PersonalEventData} from '../../../libraries/Structs/ControllerStructs';
+import {PersonalEventData} from '../../../libraries/Structs/ControllerStructs';
 import {useState} from 'react';
 import {Menu} from 'react-native-paper';
 import {Item} from 'react-navigation-header-buttons';
 import {AppIcons} from '../../../libraries/Enums/Icons';
 import React from 'react';
 import {HelpMenuItem} from '../Items/HelpMenuItem';
-import {EventDownloadMenuItem} from '../Events/Items/EventDownloadMenuItem.tsx';
 
 interface PersonalEventScreenActionsMenuProps {
   event: PersonalEventData;
 }
 
 const helpContent = [
-  'Always check the official daily printed schedule to confirm event times/locations.',
-  'Favoriting an event adds it to your schedule and gives you reminder notifications.',
-  'All events are given a corresponding forum. You can use that to discuss the event by tapping the forum button in the Menu.',
+  'Personal events are not a way to reserve space.',
+  'Confirm that the time your event is shown in Twitarr makes sense with the ships clocks.',
 ];
 
 export const PersonalEventScreenActionsMenu = (props: PersonalEventScreenActionsMenuProps) => {
