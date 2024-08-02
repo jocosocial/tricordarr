a4c1550708e2f94b9306d502503cc667b8ceadbe
Grant Cohoe <grant@grantcohoe.com>
2024-08-01 22:04:19 -0400

    add personalevent card menu

---
+++ src/components/Lists/Items/PersonalEvent/PersonalEventCardListItem.tsx
@@ -0,0 +1,35 @@
+import React, {Dispatch, SetStateAction, useState} from 'react';
+import {PersonalEventData} from '../../../../libraries/Structs/ControllerStructs';
+import {ScheduleCardMarkerType} from '../../../../libraries/Types';
+import {PersonalEventCard} from '../../../Cards/Schedule/PersonalEventCard.tsx';
+import {PersonalEventCardActionsMenu} from '../../../Menus/PersonalEvents/PersonalEventCardActionsMenu.tsx';
+
+interface PersonalEventCardListItemProps {
+  eventData: PersonalEventData;
+  onPress?: () => void;
+  marker?: ScheduleCardMarkerType;
+  setRefreshing?: Dispatch<SetStateAction<boolean>>;
+}
+
+export const PersonalEventCardListItem = (props: PersonalEventCardListItemProps) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const anchorContent = (
+    <PersonalEventCard
+      eventData={props.eventData}
+      onPress={props.onPress}
+      marker={props.marker}
+      onLongPress={() => setMenuVisible(true)}
+    />
+  );
+
+  return (
+    <PersonalEventCardActionsMenu
+      eventData={props.eventData}
+      setRefreshing={props.setRefreshing}
+      menuVisible={menuVisible}
+      setMenuVisible={setMenuVisible}
+      anchor={anchorContent}
+    />
+  );
+};
