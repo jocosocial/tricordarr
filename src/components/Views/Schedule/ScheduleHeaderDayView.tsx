7a1825bf35032d3abce4d7b974be351ef5a34cfd
Grant Cohoe <grant@grantcohoe.com>
2024-08-04 09:17:25 -0700

    working list

--- src/components/Views/Schedule/ScheduleHeaderDayView.tsx
+++ src/components/Views/Schedule/ScheduleHeaderDayView.tsx
@@ -1,10 +1,12 @@
 import {CruiseDayData} from '../../../libraries/Types';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {useStyles} from '../../Context/Contexts/StyleContext.ts';
 import {format} from 'date-fns';
 import {Text} from 'react-native-paper';
 import {useAppTheme} from '../../../styles/Theme.ts';
+import {useEventStackNavigation} from '../../Navigation/Stacks/EventStackNavigator.tsx';
+import {EventStackComponents} from '../../../libraries/Enums/Navigation.ts';
 
 interface ScheduleHeaderDayViewProps {
   cruiseDay: CruiseDayData;
@@ -14,6 +16,7 @@ interface ScheduleHeaderDayViewProps {
 export const ScheduleHeaderDayView = (props: ScheduleHeaderDayViewProps) => {
   const {commonStyles} = useStyles();
   const theme = useAppTheme();
+  const navigation = useEventStackNavigation();
   const styles = StyleSheet.create({
     view: {
       backgroundColor: props.isToday ? theme.colors.inverseSurface : theme.colors.inverseOnSurface,
@@ -34,13 +37,20 @@ export const ScheduleHeaderDayView = (props: ScheduleHeaderDayViewProps) => {
     },
   });
   return (
-    <View style={styles.view}>
-      <Text style={styles.dayText} variant={'titleLarge'}>
-        {format(props.cruiseDay.date, 'EEE')}
-      </Text>
-      <Text style={styles.dateText} variant={'bodyLarge'}>
-        {format(props.cruiseDay.date, 'MMM dd')}
-      </Text>
-    </View>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.replace(EventStackComponents.eventDayScreen, {
+          cruiseDay: props.cruiseDay.cruiseDay,
+        })
+      }>
+      <View style={styles.view}>
+        <Text style={styles.dayText} variant={'titleLarge'}>
+          {format(props.cruiseDay.date, 'EEE')}
+        </Text>
+        <Text style={styles.dateText} variant={'bodyLarge'}>
+          {format(props.cruiseDay.date, 'MMM dd')}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
