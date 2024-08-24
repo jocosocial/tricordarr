b15d065acbd28f3b89d7aa603e71ac2a300371ca
Grant Cohoe <grant@grantcohoe.com>
2024-08-24 18:01:22 -0400

    end text

--- src/components/Lists/Schedule/ScheduleFlatListBase.tsx
+++ src/components/Lists/Schedule/ScheduleFlatListBase.tsx
@@ -50,7 +50,7 @@ export const ScheduleFlatListBase = <TItem extends FezData | PersonalEventData |
     return <TimeDivider label={label} />;
   }, [items, separator]);
 
-  const renderListFooter = () => <TimeDivider label={'End'} />;
+  const renderListFooter = () => <TimeDivider label={'End of list'} />;
 
   const renderSeparatorDay = ({leadingItem}: {leadingItem: TItem}) => {
     const leadingIndex = items.indexOf(leadingItem);
