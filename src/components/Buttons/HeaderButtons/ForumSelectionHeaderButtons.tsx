8a5b8bde37b1b92a251e59f0d1e012c283279aa7
Grant Cohoe <grant@grantcohoe.com>
2024-09-03 10:21:43 -0400

    working multi select

--- src/components/Buttons/HeaderButtons/ForumSelectionHeaderButtons.tsx
+++ src/components/Buttons/HeaderButtons/ForumSelectionHeaderButtons.tsx
@@ -6,6 +6,7 @@ import {useSelection} from '../../Context/Contexts/SelectionContext.ts';
 import {ForumListData} from '../../../libraries/Structs/ControllerStructs.tsx';
 import {useForumRelationMutation} from '../../Queries/Forum/ForumThreadRelationMutations.tsx';
 import {useQueryClient} from '@tanstack/react-query';
+import {AxiosResponse} from 'axios';
 
 interface ForumSelectionHeaderButtonsProps {
   setRefreshing: Dispatch<SetStateAction<boolean>>;
@@ -19,9 +20,12 @@ export const ForumSelectionHeaderButtons = (props: ForumSelectionHeaderButtonsPr
 
   const onPress = async (relation: 'mute' | 'favorite') => {
     props.setRefreshing(true);
+
+    // https://stackoverflow.com/questions/70771324/how-to-handle-multiple-mutations-in-parallel-with-react-query
+    const mutations: Promise<AxiosResponse<void, any>>[] = [];
     selectedItems.forEach(i => {
       const relationStatus = relation === 'favorite' ? i.isFavorite : i.isMuted;
-      relationMutation.mutate(
+      const mutation = relationMutation.mutateAsync(
         {
           forumID: i.forumID,
           relationType: relation,
@@ -33,7 +37,9 @@ export const ForumSelectionHeaderButtons = (props: ForumSelectionHeaderButtonsPr
           },
         },
       );
+      mutations.push(mutation);
     });
+    await Promise.allSettled(mutations);
     const invalidationQueryKeys = ForumListData.getForumCacheKeys(props.categoryID);
     invalidationQueryKeys.forEach(key => queryClient.invalidateQueries(key));
     setEnableSelection(false);
