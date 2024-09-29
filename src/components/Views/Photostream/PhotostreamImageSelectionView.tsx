b278f167a9aee6316546a52a4f70db0569c8e781
Chall Fry <chall.fry@gmail.com>
2024-09-29 13:37:20 -0700

    Scan Photostream photos for text and remove (#192)
    Added an Android Native Module that loads in a photo, uses Google's ML Kit to scan the photo for text, and blurs out the text if any is found. The native module interfaces with RN entirely within PhotostreamImageSelectionView.tsx.
    
    All the heavy lifting is in ImageTextBlur.kt. This change does add a new library to the app, which may increase the app's size by ~4MB.
    
    A similar native module could be made for iOS, but it doesn't look like the iOS build is working.
    
    Co-authored-by: Chall Fry <chall@challfry.com>

--- src/components/Views/Photostream/PhotostreamImageSelectionView.tsx
+++ src/components/Views/Photostream/PhotostreamImageSelectionView.tsx
@@ -9,6 +9,10 @@ import {ImageButtons} from '../../Buttons/ImageButtons.tsx';
 import {AppImage} from '../../Images/AppImage.tsx';
 import {ImageQueryData} from '../../../libraries/Types';
 import {View} from 'react-native';
+import {NativeModules} from 'react-native';
+import RNFS from 'react-native-fs';
+
+const {ImageTextBlurModule} = NativeModules;
 
 export const PhotostreamImageSelectionView = () => {
   const {commonStyles} = useStyles();
@@ -16,9 +20,15 @@ export const PhotostreamImageSelectionView = () => {
   const {values, setFieldValue, isSubmitting} = useFormikContext<PhotostreamUploadData>();
 
   const processImage = (image: Image) => {
-    if (image.data) {
-      setFieldValue('image', image.data);
-    }
+    ImageTextBlurModule.blurTextInImage(image.path, newPath => {
+      RNFS.readFile(newPath, 'base64')
+        .then((blurredImageData) => {
+          setFieldValue('image', blurredImageData);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    });
   };
 
   const clearImage = () => {
@@ -28,7 +38,7 @@ export const PhotostreamImageSelectionView = () => {
   const pickImage = async () => {
     try {
       const image = await ImagePicker.openPicker({
-        includeBase64: true,
+        includeBase64: false,
         mediaType: 'photo',
       });
       processImage(image);
@@ -42,7 +52,7 @@ export const PhotostreamImageSelectionView = () => {
     console.log('[UserProfileAvatar.tsx] Camera permission is', permissionStatus);
     try {
       const image = await ImagePicker.openCamera({
-        includeBase64: true,
+        includeBase64: false,
         mediaType: 'photo',
       });
       processImage(image);
