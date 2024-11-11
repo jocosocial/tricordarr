c2d465dcaa8b9401abc3ce4071ebe523cbcf9da5
Grant Cohoe <grant@grantcohoe.com>
2024-11-11 17:03:57 -0500

    add some intense query analysis capabilities

---
+++ src/components/Screens/Settings/Developer/QueryDataSettingsScreen.tsx
@@ -0,0 +1,70 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {AppView} from '../../../Views/AppView.tsx';
+import {RefreshControl, ScrollView} from 'react-native';
+import {Text} from 'react-native-paper';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {SettingsStackParamList} from '../../../Navigation/Stacks/SettingsStackNavigator.tsx';
+import {SettingsStackScreenComponents} from '../../../../libraries/Enums/Navigation.ts';
+import {Query, useQueryClient} from '@tanstack/react-query';
+import {PaddedContentView} from '../../../Views/Content/PaddedContentView.tsx';
+import JSONTree from 'react-native-json-tree';
+import {PrimaryActionButton} from '../../../Buttons/PrimaryActionButton.tsx';
+
+export type Props = NativeStackScreenProps<
+  SettingsStackParamList,
+  SettingsStackScreenComponents.queryDataSettingsScreen
+>;
+
+export const QueryDataSettingsScreen = ({route}: Props) => {
+  const queryClient = useQueryClient();
+  const [query, setQuery] = useState<Query>();
+  const [refreshing, setRefreshing] = useState(false);
+
+  // const refresh = useCallback(() => {
+  //   setRefreshing(true);
+  //   const result = queryClient.getQueryCache().get(route.params.queryHash);
+  //   setQuery(result);
+  //   setRefreshing(false);
+  // }, [queryClient, route.params.queryHash]);
+
+  // useEffect(() => {
+  //   refresh();
+  // }, [refresh]);
+
+  const refresh = async () => {
+    setRefreshing(true);
+    const result = queryClient.getQueryCache().get(route.params.queryHash);
+    setQuery(result);
+    setRefreshing(false);
+  };
+
+  const refetch = async () => {
+    await query?.fetch();
+    await refresh();
+  };
+
+  useEffect(() => {
+    const result = queryClient.getQueryCache().get(route.params.queryHash);
+    setQuery(result);
+  }, [queryClient, route.params.queryHash]);
+
+  if (!query) {
+    return (
+      <AppView>
+        <PaddedContentView>
+          <Text>No Query State</Text>
+        </PaddedContentView>
+      </AppView>
+    );
+  }
+
+  return (
+    <AppView>
+      <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refetch} />}>
+        <ScrollView horizontal={true}>
+          <JSONTree data={query.state as any} hideRoot={true} shouldExpandNode={() => true} />
+        </ScrollView>
+      </ScrollView>
+    </AppView>
+  );
+};
