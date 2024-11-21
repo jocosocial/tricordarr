86804a4f6fa15a79def8f0fb28cf263f26117a9f
Grant Cohoe <grant@grantcohoe.com>
2024-11-20 21:45:15 -0500

    lfg settings

--- src/components/Screens/LFG/LfgSettingsScreen.tsx
+++ src/components/Screens/LFG/LfgSettingsScreen.tsx
@@ -1,7 +1,7 @@
 import {AppView} from '../../Views/AppView';
 import {ScrollingContentView} from '../../Views/Content/ScrollingContentView';
 import {PaddedContentView} from '../../Views/Content/PaddedContentView';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Formik} from 'formik';
 import {useConfig} from '../../Context/Contexts/ConfigContext';
 import {useStyles} from '../../Context/Contexts/StyleContext';
@@ -12,6 +12,9 @@ import {HelperText, SegmentedButtons, Text} from 'react-native-paper';
 import {LfgStackComponents} from '../../../libraries/Enums/Navigation';
 import {SegmentedButtonType} from '../../../libraries/Types';
 import {AppIcons} from '../../../libraries/Enums/Icons';
+import {ListSubheader} from '../../Lists/ListSubheader.tsx';
+import {ListSection} from '../../Lists/ListSection.tsx';
+import {check as checkPermission, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 export const LfgSettingsScreen = () => {
   const {appConfig, updateAppConfig} = useConfig();
@@ -19,6 +22,7 @@ export const LfgSettingsScreen = () => {
   const {setLfgHidePastFilter} = useFilter();
   const {commonStyles} = useStyles();
   const [defaultScreen, setDefaultScreen] = useState(appConfig.schedule.defaultLfgScreen);
+  const [permissionStatus, setPermissionStatus] = useState('Unknown');
 
   const handleHidePastLfgs = () => {
     const newValue = !appConfig.schedule.hidePastLfgs;
@@ -62,22 +66,51 @@ export const LfgSettingsScreen = () => {
     setDefaultScreen(value as LfgStackComponents);
   };
 
+  const toggleLFGUnreadNotifications = () => {
+    updateAppConfig({
+      ...appConfig,
+      pushNotifications: {
+        ...appConfig.pushNotifications,
+        fezUnreadMsg: !appConfig.pushNotifications.fezUnreadMsg,
+      },
+    });
+  };
+
+  const toggleLfgJoinedNotifications = () => {
+    updateAppConfig({
+      ...appConfig,
+      pushNotifications: {
+        ...appConfig.pushNotifications,
+        joinedLFGStarting: !appConfig.pushNotifications.joinedLFGStarting,
+      },
+    });
+  };
+
+  useEffect(() => {
+    checkPermission(PERMISSIONS.ANDROID.POST_NOTIFICATIONS).then(status => {
+      setPermissionStatus(status);
+    });
+  }, []);
+
   return (
     <AppView>
       <ScrollingContentView>
         <PaddedContentView padSides={false}>
           <Formik initialValues={{}} onSubmit={() => {}}>
             <View>
-              <BooleanField
-                name={'hidePastLfgs'}
-                label={'Hide Past LFGs by Default'}
-                onPress={handleHidePastLfgs}
-                style={commonStyles.paddingHorizontal}
-                helperText={
-                  'Default to not showing LFGs that have already happened. You can still use the filters to view them.'
-                }
-                value={hidePastLfgs}
-              />
+              <ListSection>
+                <ListSubheader>General</ListSubheader>
+                <BooleanField
+                  name={'hidePastLfgs'}
+                  label={'Hide Past LFGs by Default'}
+                  onPress={handleHidePastLfgs}
+                  style={commonStyles.paddingHorizontal}
+                  helperText={
+                    'Default to not showing LFGs that have already happened. You can still use the filters to view them.'
+                  }
+                  value={hidePastLfgs}
+                />
+              </ListSection>
             </View>
           </Formik>
         </PaddedContentView>
@@ -87,6 +120,35 @@ export const LfgSettingsScreen = () => {
           <HelperText style={commonStyles.onBackground} type={'info'}>
             Changing this setting requires an app restart.
           </HelperText>
+        </PaddedContentView>
+        <PaddedContentView padSides={false}>
+          <Formik initialValues={{}} onSubmit={() => {}}>
+            <View>
+              <ListSection>
+                <ListSubheader>Push Notifications</ListSubheader>
+                <BooleanField
+                  key={'fezUnreadMsg'}
+                  name={'fezUnreadMsg'}
+                  label={'LFG Posts'}
+                  value={appConfig.pushNotifications.fezUnreadMsg}
+                  onPress={toggleLFGUnreadNotifications}
+                  disabled={permissionStatus !== RESULTS.GRANTED}
+                  helperText={"New unread chat public messages posted in an LFG you've joined or that you own."}
+                  style={commonStyles.paddingHorizontal}
+                />
+                <BooleanField
+                  key={'joinedLFGStarting'}
+                  name={'joinedLFGStarting'}
+                  label={'Joined LFG Reminders'}
+                  value={appConfig.pushNotifications.joinedLFGStarting}
+                  onPress={toggleLfgJoinedNotifications}
+                  disabled={permissionStatus !== RESULTS.GRANTED}
+                  helperText={'Reminder that a joined LFG is starting Soon™.'}
+                  style={commonStyles.paddingHorizontal}
+                />
+              </ListSection>
+            </View>
+          </Formik>
         </PaddedContentView>
       </ScrollingContentView>
     </AppView>
