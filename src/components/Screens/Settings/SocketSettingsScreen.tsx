724a6af4aa80baa690b6f947d00f789f30b3f1f3
Grant Cohoe <grant@grantcohoe.com>
2024-11-20 21:03:25 -0500

    rework socket settings

--- src/components/Screens/Settings/SocketSettingsScreen.tsx
+++ src/components/Screens/Settings/SocketSettingsScreen.tsx
@@ -4,12 +4,14 @@ import {ScrollingContentView} from '../../Views/Content/ScrollingContentView';
 import {PaddedContentView} from '../../Views/Content/PaddedContentView';
 import {SettingSwitch} from '../../Switches/SettingSwitch';
 import {useConfig} from '../../Context/Contexts/ConfigContext';
-import {DataTable, Divider, Text} from 'react-native-paper';
+import {DataTable, Text} from 'react-native-paper';
 import {SocketControlView} from '../../Views/SocketControlView';
 import {RefreshControl} from 'react-native';
 import {useSocket} from '../../Context/Contexts/SocketContext';
 import {WebSocketState} from '../../../libraries/Network/Websockets';
 import {SettingDataTableRow} from '../../DataTables/SettingDataTableRow';
+import {ListSection} from '../../Lists/ListSection.tsx';
+import {ListSubheader} from '../../Lists/ListSubheader.tsx';
 
 export const SocketSettingsScreen = () => {
   const {appConfig, updateAppConfig} = useConfig();
@@ -45,48 +47,51 @@ export const SocketSettingsScreen = () => {
 
   return (
     <AppView>
-      <ScrollingContentView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-        <PaddedContentView>
-          <Text variant={'titleMedium'}>Notification Socket Status</Text>
-          <DataTable>
-            <SettingDataTableRow title={'Socket State'} value={notificationSocketState} />
-          </DataTable>
-        </PaddedContentView>
-        <Divider bold={true} />
-        <PaddedContentView padTop={true}>
-          <Text variant={'titleMedium'}>Enable</Text>
-          <SettingSwitch
-            title={'Notification Socket'}
-            value={appConfig.enableNotificationSocket}
-            onPress={toggleNotificationSocket}
-            description={
-              'Used for general purpose notifications across all app features. Not used for background connection.'
-            }
-          />
-          <SettingSwitch
-            title={'Fez Sockets'}
-            value={appConfig.enableFezSocket}
-            onPress={toggleFezSocket}
-            description={
-              'Opened when a fez is loaded. Provides membership change messages in addition to content updates.'
-            }
-          />
-        </PaddedContentView>
-        <Divider bold={true} />
-        <PaddedContentView padTop={true}>
-          <Text variant={'titleMedium'}>Control</Text>
-          <SocketControlView
-            title={'Notification Socket'}
-            disabled={!appConfig.enableNotificationSocket}
-            onReset={closeNotificationSocket}
-          />
-          <Text variant={'bodyMedium'}>
-            This triggers a socket close and open. The socket can be disabled or more persistently closed by disabling
-            it above.
-          </Text>
-          <Text variant={'bodyMedium'}>You may need to refresh this screen for current state.</Text>
-        </PaddedContentView>
-        <Divider bold={true} />
+      <ScrollingContentView
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        isStack={true}>
+        <ListSection>
+          <ListSubheader>Notification Socket</ListSubheader>
+          <PaddedContentView padBottom={false}>
+            <DataTable>
+              <SettingDataTableRow title={'Socket State'} value={notificationSocketState} />
+            </DataTable>
+          </PaddedContentView>
+          <PaddedContentView padSides={false}>
+            <SettingSwitch
+              title={'Enable'}
+              value={appConfig.enableNotificationSocket}
+              onPress={toggleNotificationSocket}
+              description={
+                'Used for general purpose notifications across all app features. Not used for background connection.'
+              }
+            />
+          </PaddedContentView>
+          <PaddedContentView>
+            <SocketControlView
+              title={'Reset Notification Socket'}
+              disabled={!appConfig.enableNotificationSocket}
+              onReset={closeNotificationSocket}
+            />
+            <Text variant={'bodyMedium'}>
+              This triggers a socket close and open. The socket can be disabled or more persistently closed by disabling
+              it above. You may need to refresh this screen for current state.
+            </Text>
+          </PaddedContentView>
+        </ListSection>
+        <ListSection>
+          <ListSubheader>Fez Socket</ListSubheader>
+          <PaddedContentView padSides={false}>
+            <SettingSwitch
+              title={'Enable'}
+              value={appConfig.enableFezSocket}
+              onPress={toggleFezSocket}
+              description={
+                'Opened when a fez is loaded. Provides membership change messages in addition to content updates.'
+              }
+            />
+          </PaddedContentView>
+        </ListSection>
       </ScrollingContentView>
     </AppView>
   );
--- src/components/Switches/SettingSwitch.tsx
+++ src/components/Switches/SettingSwitch.tsx
@@ -1,4 +1,4 @@
-import {Switch, View} from 'react-native';
+import {StyleProp, Switch, View, ViewStyle, StyleSheet} from 'react-native';
 import {Text, TouchableRipple} from 'react-native-paper';
 import React from 'react';
 import {useStyles} from '../Context/Contexts/StyleContext';
@@ -9,14 +9,27 @@ interface SettingSwitchProps {
   onPress: () => void;
   description?: string;
   disabled?: boolean;
+  style?: StyleProp<ViewStyle>;
 }
 
-export const SettingSwitch = ({title, value, onPress, description, disabled}: SettingSwitchProps) => {
+export const SettingSwitch = ({title, value, onPress, description, disabled, style}: SettingSwitchProps) => {
   const {commonStyles} = useStyles();
+
+  const styles = StyleSheet.create({
+    wrapper: {
+      ...commonStyles.marginTop,
+      ...commonStyles.paddingHorizontal,
+      ...(style as ViewStyle),
+    },
+    inner: {
+      ...commonStyles.booleanSettingRowView,
+    },
+  });
+
   return (
-    <TouchableRipple style={commonStyles.marginTop} onPress={onPress} disabled={disabled}>
+    <TouchableRipple style={styles.wrapper} onPress={onPress} disabled={disabled}>
       <View>
-        <View style={commonStyles.booleanSettingRowView}>
+        <View style={styles.inner}>
           <Text>{title}</Text>
           <Switch value={value} onValueChange={onPress} disabled={disabled} />
         </View>
