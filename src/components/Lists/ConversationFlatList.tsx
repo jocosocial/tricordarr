d10dfc1fd00e424c5d62f6456d17adaaf2ef598f
Grant Cohoe <grant@grantcohoe.com>
2024-11-23 14:11:49 -0500

    build the new generic list

---
+++ src/components/Lists/ConversationFlatList.tsx
@@ -0,0 +1,197 @@
+import {
+  FlatList,
+  ListRenderItem,
+  ListRenderItemInfo,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {FloatingScrollButton} from '../Buttons/FloatingScrollButton.tsx';
+import {AppIcons} from '../../libraries/Enums/Icons.ts';
+import React, {useCallback, useState} from 'react';
+import {FloatingScrollButtonPosition} from '../../libraries/Types';
+import {IconSource} from 'react-native-paper/lib/typescript/components/Icon';
+import {useStyles} from '../Context/Contexts/StyleContext.ts';
+import ArrayLike = jasmine.ArrayLike;
+
+interface ConversationFlatListProps<TItem> {
+  floatingScrollButtonPosition?: FloatingScrollButtonPosition;
+  floatingScrollButtonIcon?: IconSource;
+  invertList?: boolean;
+  flatListRef: React.RefObject<FlatList<TItem>>;
+  hasPreviousPage?: boolean;
+  hasNextPage?: boolean;
+  handleLoadPrevious: () => void;
+  handleLoadNext: () => void;
+  onEndReachedThreshold?: number;
+  onStartReachedThreshold?: number;
+  keyExtractor?: (item: TItem, index: number) => string;
+  initialScrollIndex?: number;
+  renderListHeader: () => React.JSX.Element;
+  renderListFooter: () => React.JSX.Element;
+  renderItem: ListRenderItem<TItem>;
+  data: ArrayLike<TItem>;
+}
+
+export const ConversationFlatList = <TItem,>({
+  floatingScrollButtonPosition,
+  floatingScrollButtonIcon = AppIcons.scrollDown,
+  invertList = true,
+  flatListRef,
+  hasNextPage,
+  hasPreviousPage,
+  handleLoadPrevious,
+  handleLoadNext,
+  onStartReachedThreshold = 1,
+  onEndReachedThreshold = 1,
+  keyExtractor,
+  initialScrollIndex = 0,
+  renderListHeader,
+  renderListFooter,
+  renderItem,
+  data,
+}: ConversationFlatListProps<TItem>) => {
+  const {commonStyles, styleDefaults} = useStyles();
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  const [contentHeight, setContentHeight] = useState(0);
+  const [itemHeights, setItemHeights] = useState<number[]>([]);
+
+  const styles = StyleSheet.create({
+    flatList: {
+      ...commonStyles.paddingHorizontal,
+      ...(invertList ? commonStyles.verticallyInverted : undefined),
+    },
+    itemContainerView: {
+      ...(invertList ? commonStyles.verticallyInverted : undefined),
+    },
+  });
+
+  const handleScrollButtonPress = useCallback(() => {
+    if (invertList) {
+      console.log('[ConversationFlatList.tsx] scrolling to offset 0');
+      flatListRef.current?.scrollToOffset({offset: 0, animated: true});
+    } else {
+      console.log('[ConversationFlatList.tsx] scrolling to end');
+      flatListRef.current?.scrollToOffset({offset: contentHeight, animated: true});
+    }
+  }, [contentHeight, flatListRef, invertList]);
+
+  const onScroll = useCallback(
+    (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+      if (invertList) {
+        setShowScrollButton(event.nativeEvent.contentOffset.y > styleDefaults.listScrollThreshold);
+      } else {
+        setShowScrollButton(
+          event.nativeEvent.contentSize.height - event.nativeEvent.contentOffset.y >
+            styleDefaults.listScrollThreshold * 2,
+        );
+      }
+    },
+    [invertList, styleDefaults.listScrollThreshold],
+  );
+
+  const onContentSizeChange = useCallback((w: number, h: number) => {
+    setContentHeight(h);
+  }, []);
+
+  /**
+   * If the forum has been fully read and you only have the latest very small
+   * page, it can leave the list in a situation where the onEndReached hook
+   * never fires. This is particularly problematic in inverted lists because
+   * you can be left with one or two posts and a header that says Loading.
+   * So this jogs its memory. A potential future optimization can be to trigger
+   * this only if the pageSize is small (like <=10).
+   */
+  const onLayout = useCallback(() => {
+    if (invertList && hasPreviousPage && handleLoadPrevious) {
+      handleLoadPrevious();
+    } else if (!invertList && hasNextPage && handleLoadNext) {
+      handleLoadNext();
+    }
+  }, [handleLoadNext, handleLoadPrevious, hasNextPage, hasPreviousPage, invertList]);
+
+  const getItemHeight = useCallback(
+    (index: number) => {
+      return itemHeights[index] || 0;
+    },
+    [itemHeights],
+  );
+
+  // @TODO factor in separators.
+  const getItemOffset = useCallback(
+    (index: number) => {
+      if (itemHeights[index] === undefined) {
+        return 0;
+      }
+      return itemHeights.slice(0, index).reduce((previousValue, currentItem) => previousValue + currentItem, 0);
+    },
+    [itemHeights],
+  );
+
+  const getItemLayout = useCallback(
+    (data: ArrayLike<TItem> | null | undefined, index: number) => ({
+      length: getItemHeight(index),
+      offset: getItemOffset(index),
+      index,
+    }),
+    [getItemHeight, getItemOffset],
+  );
+
+  const renderItemInternal = useCallback(
+    (renderItemProps: ListRenderItemInfo<TItem>) => {
+      return (
+        <View
+          style={styles.itemContainerView}
+          onLayout={event => {
+            // Doing this without the variable blows up with a null value. Wonder
+            // if the setter callback is resetting the event context? /shrug.
+            const layout = event.nativeEvent.layout;
+            // console.log(`index=${index}`, 'layout', layout, `post=${item.text.substring(0, 9)}`);
+            setItemHeights(prevData => {
+              return [...prevData, layout.height];
+            });
+          }}>
+          {renderItem(renderItemProps)}
+        </View>
+      );
+    },
+    [renderItem, styles.itemContainerView],
+  );
+
+  return (
+    <>
+      <FlatList
+        ref={flatListRef}
+        data={data}
+        renderItem={renderItemInternal}
+        onContentSizeChange={onContentSizeChange}
+        onScroll={onScroll}
+        onLayout={onLayout}
+        style={styles.flatList}
+        onStartReachedThreshold={onStartReachedThreshold}
+        onEndReachedThreshold={onEndReachedThreshold}
+        keyExtractor={keyExtractor}
+        onScrollToIndexFailed={info => console.warn('scroll failed', info)}
+        // Just setting initialScrollIndex is causing the list to be empty. Allegedly this is
+        // because FlatList skips rendering for invisible items.
+        // disableVirtualization={true} made this work, though I feel like I'm going to
+        // regret it at some point.
+        initialScrollIndex={initialScrollIndex}
+        disableVirtualization={true}
+        getItemLayout={getItemLayout}
+        onStartReached={invertList ? handleLoadNext : handleLoadPrevious}
+        onEndReached={invertList ? handleLoadPrevious : handleLoadNext}
+        ListFooterComponent={invertList ? renderListHeader : renderListFooter}
+        ListHeaderComponent={invertList ? renderListFooter : renderListHeader}
+      />
+      {showScrollButton && (
+        <FloatingScrollButton
+          icon={floatingScrollButtonIcon}
+          onPress={handleScrollButtonPress}
+          displayPosition={floatingScrollButtonPosition}
+        />
+      )}
+    </>
+  );
+};
