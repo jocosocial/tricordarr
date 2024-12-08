28a5f6964fed18db65b1f1d1e53eece98572931d
Grant Cohoe <grant@grantcohoe.com>
2024-12-08 12:53:37 -0500

    photostream help

--- src/components/Screens/Photostream/PhotostreamHelpScreen.tsx
+++ src/components/Screens/Photostream/PhotostreamHelpScreen.tsx
@@ -2,33 +2,33 @@ import React from 'react';
 import {AppView} from '../../Views/AppView.tsx';
 import {ScrollingContentView} from '../../Views/Content/ScrollingContentView.tsx';
 import {PaddedContentView} from '../../Views/Content/PaddedContentView.tsx';
-import {Text} from 'react-native-paper';
-import {StyleSheet} from 'react-native';
-import {useStyles} from '../../Context/Contexts/StyleContext.ts';
+import {ListTitleView} from '../../Views/ListTitleView.tsx';
+import {HelpParagraphText} from '../../Text/Help/HelpParagraphText.tsx';
 
 export const PhotostreamHelpScreen = () => {
-  const {commonStyles} = useStyles();
-
-  const styles = StyleSheet.create({
-    title: {
-      ...commonStyles.bold,
-      ...commonStyles.marginBottomSmall,
-    },
-    paragraph: {
-      ...commonStyles.marginBottomSmall,
-    },
-  });
-
   return (
     <AppView>
-      <ScrollingContentView>
-        <PaddedContentView>
-          <Text variant={'titleLarge'} style={styles.title}>
-            Photo Stream
-          </Text>
-          <Text style={styles.paragraph}>
+      <ScrollingContentView isStack={true}>
+        <ListTitleView title={'General'} />
+        <PaddedContentView padTop={true}>
+          <HelpParagraphText>
             Its a stream. Of photos. A photostream. Share what you're seeing on the ship.
-          </Text>
+          </HelpParagraphText>
+        </PaddedContentView>
+        <ListTitleView title={'Restrictions'} />
+        <PaddedContentView padTop={true}>
+          <HelpParagraphText>You can only post one image every five minutes. Choose wisely!</HelpParagraphText>
+          <HelpParagraphText>Posted images cannot be deleted other than by moderators.</HelpParagraphText>
+          <HelpParagraphText>
+            Images cannot contain text. Any text detected in the image will be automatically blurred prior to upload.
+            You will be able to review your image before posting it.
+          </HelpParagraphText>
+          <HelpParagraphText>
+            You must tag either a location or event for your photo. This is to help others see what's going on.
+          </HelpParagraphText>
+          <HelpParagraphText>
+            There are intentionally no comments or reactions. Just a stream of cool things happening on board.
+          </HelpParagraphText>
         </PaddedContentView>
       </ScrollingContentView>
     </AppView>
