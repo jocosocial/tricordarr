885ad96acc2e47bb0dd782a4242fc954dc3e2c49
Grant Cohoe <grant@grantcohoe.com>
2024-12-08 14:43:28 -0500

    bold?

--- src/components/Lists/ListSubheader.tsx
+++ src/components/Lists/ListSubheader.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import {PropsWithChildren} from 'react';
 import {List} from 'react-native-paper';
 import {useStyles} from '../Context/Contexts/StyleContext';
-import {StyleSheet} from 'react-native';
+import {StyleProp, StyleSheet, TextStyle} from 'react-native';
 
-export const ListSubheader = ({children}: PropsWithChildren) => {
+interface ListSubheaderProps extends PropsWithChildren {
+  style?: StyleProp<TextStyle>;
+}
+
+export const ListSubheader = ({children, style}: ListSubheaderProps) => {
   const {commonStyles} = useStyles();
   const styles = StyleSheet.create({
     subheader: {
       ...commonStyles.surfaceVariant,
+      ...(style as TextStyle),
     },
   });
   return <List.Subheader style={styles.subheader}>{children}</List.Subheader>;
--- src/components/Views/Help/HelpChapterTitleView.tsx
+++ src/components/Views/Help/HelpChapterTitleView.tsx
@@ -1,6 +1,6 @@
 import {ListSubheader} from '../../Lists/ListSubheader.tsx';
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useStyles} from '../../Context/Contexts/StyleContext.ts';
 
 interface HelpChapterTitleView {
@@ -12,8 +12,15 @@ interface HelpChapterTitleView {
  */
 export const HelpChapterTitleView = (props: HelpChapterTitleView) => {
   const {commonStyles} = useStyles();
+
+  const styles = StyleSheet.create({
+    container: commonStyles.marginBottom,
+    // I actually like this less.
+    // text: commonStyles.bold,
+  });
+
   return (
-    <View style={commonStyles.marginBottom}>
+    <View style={styles.container}>
       <ListSubheader>{props.title}</ListSubheader>
     </View>
   );
