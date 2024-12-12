fa947b94666de6da0bf673b3db6559aa4a5c34ae
Grant Cohoe <grant@grantcohoe.com>
2024-12-12 15:32:57 -0500

    well, it kinda works

--- src/components/Lists/Items/MicroKaraokeSongListItem.tsx
+++ src/components/Lists/Items/MicroKaraokeSongListItem.tsx
@@ -1,18 +1,52 @@
-import {List, Text} from 'react-native-paper';
-import React from 'react';
+import {IconButton, List, ProgressBar, Text} from 'react-native-paper';
+import React, {useState} from 'react';
 import {MicroKaraokeCompletedSong} from '../../../libraries/Structs/ControllerStructs.tsx';
 import {View, StyleSheet} from 'react-native';
 import {RelativeTimeTag} from '../../Text/Tags/RelativeTimeTag.tsx';
 import {useStyles} from '../../Context/Contexts/StyleContext.ts';
 import {MainStackComponents, useMainStack} from '../../Navigation/Stacks/MainStackNavigator.tsx';
+import {AppIcons} from '../../../libraries/Enums/Icons.ts';
+import {useMicroKaraokeSongQuery} from '../../Queries/MicroKaraoke/MicroKaraokeQueries.ts';
+import RNFS from 'react-native-fs';
+import {useErrorHandler} from '../../Context/Contexts/ErrorHandlerContext.ts';
 
 interface MicroKaraokeSongListItemProps {
   mkSong: MicroKaraokeCompletedSong;
 }
 
+const downloadFile = async (url: string) => {
+  const fileName = url.split('/').pop();
+  if (!fileName) {
+    throw Error(`Unable to determine fileName from url: ${url}`);
+  }
+  const destinationPath = `${RNFS.DocumentDirectoryPath}/${fileName}`;
+
+  const exists = await RNFS.exists(destinationPath);
+  if (exists) {
+    console.log(`File ${destinationPath} already exists`);
+    return destinationPath;
+  }
+
+  const result = await RNFS.downloadFile({
+    fromUrl: url,
+    toFile: destinationPath,
+  }).promise;
+  console.log('Got status', result.statusCode);
+
+  if (result.statusCode === 200) {
+    console.log(`Successfully saved ${url} to ${destinationPath}`);
+  }
+  return destinationPath;
+};
+
 export const MicroKaraokeSongListItem = ({mkSong}: MicroKaraokeSongListItemProps) => {
   const {commonStyles} = useStyles();
   const mainStack = useMainStack();
+  const {data, refetch, isFetching} = useMicroKaraokeSongQuery(mkSong.songID, false);
+  const [downloadedSnippets, setDownloadedSnippets] = useState(0);
+  const [downloading, setDownloading] = useState(false);
+  const [availableSnippets, setAvailableSnippets] = useState(0);
+  const {setErrorMessage} = useErrorHandler();
 
   const styles = StyleSheet.create({
     title: commonStyles.bold,
@@ -36,5 +70,62 @@ export const MicroKaraokeSongListItem = ({mkSong}: MicroKaraokeSongListItemProps
       songID: mkSong.songID,
     });
 
-  return <List.Item title={mkSong.songName} titleStyle={styles.title} description={getDescription} onPress={onPress} />;
+  const onDownload = async () => {
+    setDownloading(true);
+    setDownloadedSnippets(0);
+    await refetch();
+    if (data) {
+      console.log(`There are ${data.snippetVideoURLs.length} snippets available`);
+      setAvailableSnippets(data.snippetVideoURLs.length);
+      for (const snippetVideoURL of data.snippetVideoURLs) {
+        try {
+          await downloadFile(snippetVideoURL);
+          setDownloadedSnippets(prev => prev + 1);
+        } catch (error) {
+          console.error(`Error with url ${snippetVideoURL}`);
+          console.error(error);
+          setErrorMessage(error);
+        }
+      }
+    }
+    setDownloading(false);
+  };
+
+  const onClear = async () => {
+    if (data) {
+      const results = data.snippetVideoURLs.map(async snippetVideoURL => {
+        const fileName = snippetVideoURL.split('/').pop();
+        if (!fileName) {
+          return;
+        }
+        console.log(`Clearing ${fileName}`);
+        try {
+          await RNFS.unlink(`${RNFS.DocumentDirectoryPath}/${fileName}`);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+      await Promise.all(results);
+    }
+  };
+
+  const getDownloadButton = () => (
+    <IconButton disabled={downloading} loading={downloading} icon={AppIcons.download} onPress={onDownload} />
+  );
+  const getClearButton = () => <IconButton icon={AppIcons.delete} onPress={onClear} />;
+  console.log('Downloaded', downloadedSnippets, 'Available', availableSnippets);
+
+  return (
+    <View>
+      <List.Item
+        title={mkSong.songName}
+        titleStyle={styles.title}
+        description={getDescription}
+        onPress={onPress}
+        right={getDownloadButton}
+        left={getClearButton}
+      />
+      {downloading && availableSnippets !== 0 && <ProgressBar progress={downloadedSnippets / availableSnippets} />}
+    </View>
+  );
 };
