1bb41cb747cb0760bf72a84a04607f99b099efbf
Grant Cohoe <grant@grantcohoe.com>
2024-12-12 19:10:13 -0500

    a bunch of dumb functions

--- src/components/Views/Today/TodayNextAppointmentView.tsx
+++ src/components/Views/Today/TodayNextAppointmentView.tsx
@@ -4,19 +4,62 @@ import React from 'react';
 import {useUserNotificationDataQuery} from '../../Queries/Alert/NotificationQueries.ts';
 import {NextLFGCard} from '../../Cards/MainScreen/NextLFGCard.tsx';
 
+interface NextEvent {
+  startTime: Date;
+  id: string;
+  type: 'lfg' | 'event';
+}
+
 export const TodayNextAppointmentView = () => {
-  const {data: userNotificationData} = useUserNotificationDataQuery();
+  const {data} = useUserNotificationDataQuery();
+
+  if (!data) {
+    return <></>;
+  }
+
+  const nextEvent: NextEvent | undefined =
+    data.nextFollowedEventTime && data.nextFollowedEventID
+      ? {
+          startTime: new Date(data.nextFollowedEventTime),
+          id: data.nextFollowedEventID,
+          type: 'event',
+        }
+      : undefined;
+  // LFG also includes PersonalEvent now.
+  const nextLfg: NextEvent | undefined =
+    data.nextJoinedLFGTime && data.nextJoinedLFGID
+      ? {
+          startTime: new Date(data.nextJoinedLFGTime),
+          id: data.nextJoinedLFGID,
+          type: 'lfg',
+        }
+      : undefined;
+
+  const getNextAppointment = () => {
+    if (nextEvent) {
+      if (nextLfg) {
+        return [nextEvent, nextLfg].sort((a, b) => a.startTime.getTime() - b.startTime.getTime())[0];
+      }
+      return nextEvent;
+    } else if (nextLfg) {
+      return nextLfg;
+    }
+    return;
+  };
+
+  const nextAppointment = getNextAppointment();
+  console.log(nextAppointment);
 
   return (
     <>
-      {userNotificationData?.nextFollowedEventID && (
+      {nextAppointment?.type === 'event' && (
         <PaddedContentView>
-          <NextEventCard eventID={userNotificationData.nextFollowedEventID} />
+          <NextEventCard eventID={nextAppointment.id} />
         </PaddedContentView>
       )}
-      {userNotificationData?.nextJoinedLFGID && (
+      {nextAppointment?.type === 'lfg' && (
         <PaddedContentView>
-          <NextLFGCard lfgID={userNotificationData.nextJoinedLFGID} />
+          <NextLFGCard lfgID={nextAppointment.id} />
         </PaddedContentView>
       )}
     </>
