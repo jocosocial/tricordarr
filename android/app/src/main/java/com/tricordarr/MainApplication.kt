d3ddf54483065a3256e48c97c55e8f338c94dd60
Grant Cohoe <grant@grantcohoe.com>
2024-12-21 14:42:12 -0500

    upgrade to react native 0.76

--- android/app/src/main/java/com/tricordarr/MainApplication.kt
+++ android/app/src/main/java/com/tricordarr/MainApplication.kt
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+// https://reactnative.dev/blog/2024/10/23/release-0.76-new-architecture#breaking-changes-1
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
@@ -35,7 +37,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
