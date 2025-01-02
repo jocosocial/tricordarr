bafddd5c0ce7ee07f6ed6be0e37079755215acb6
Grant Cohoe <grant@grantcohoe.com>
2025-01-01 23:42:26 -0500

    fix the list issue

--- src/components/Banners/PostAsUserBanner.tsx
+++ src/components/Banners/PostAsUserBanner.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import {Banner, Text} from 'react-native-paper';
 import {usePrivilege} from '../Context/Contexts/PrivilegeContext';
-import {useAppTheme} from '../../styles/Theme';
 import {useStyles} from '../Context/Contexts/StyleContext.ts';
 import {StyleSheet} from 'react-native';
 
 export const PostAsUserBanner = () => {
   const {asPrivilegedUser} = usePrivilege();
-  const theme = useAppTheme();
   const {commonStyles} = useStyles();
 
   const styles = StyleSheet.create({
     banner: {
       ...commonStyles.errorContainer,
-      ...commonStyles.safeMarginTop,
     },
     text: commonStyles.errorContainer,
   });
