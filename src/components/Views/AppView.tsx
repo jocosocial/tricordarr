9ef2cebd7948f8725f20b020e81a15c53f40952a
Grant Cohoe <grant@grantcohoe.com>
2025-01-03 00:06:19 -0500

    fix android 15 keyboard behavior

--- src/components/Views/AppView.tsx
+++ src/components/Views/AppView.tsx
@@ -1,5 +1,5 @@
 import React, {PropsWithChildren} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {KeyboardAvoidingView, StyleSheet, View} from 'react-native';
 import {Portal} from 'react-native-paper';
 import {ErrorSnackbar} from '../Snackbars/ErrorSnackbar';
 import {ErrorBanner} from '../Banners/ErrorBanner.tsx';
@@ -37,18 +37,30 @@ export const AppView = ({children, safeEdges}: AppViewProps) => {
       paddingLeft: safeEdges?.includes('left') ? insets.left : undefined,
       paddingRight: safeEdges?.includes('right') ? insets.right : undefined,
     },
+    keyboardView: {
+      ...commonStyles.flex,
+    },
   });
+
+  // The KeyboardAvoiding stuff came from
+  // https://stackoverflow.com/questions/43854912/react-native-keyboardavoidingview-covers-last-text-input
+  // https://github.com/react-native-community/discussions-and-proposals/discussions/827
 
   return (
     <View style={styles.appView}>
-      <Portal>
-        <ErrorBanner />
-        <AppModal />
-        <ErrorSnackbar />
-        <InfoSnackbar />
-      </Portal>
-      {disruptionDetected && <ConnectionDisruptedView />}
-      {children}
+      <KeyboardAvoidingView
+        style={styles.keyboardView}
+        behavior={'padding'}
+        keyboardVerticalOffset={insets.top + insets.bottom}>
+        <Portal>
+          <ErrorBanner />
+          <AppModal />
+          <ErrorSnackbar />
+          <InfoSnackbar />
+        </Portal>
+        {disruptionDetected && <ConnectionDisruptedView />}
+        {children}
+      </KeyboardAvoidingView>
       <UnsavedChangesView isVisible={hasUnsavedWork} />
     </View>
   );
