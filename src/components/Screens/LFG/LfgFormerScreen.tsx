2dae6c8efe6fcee79dae0545d8e5dbbfb331d535
Grant Cohoe <grant@grantcohoe.com>
2025-01-04 13:32:53 -0500

    favorite text and former lfg fab

--- src/components/Screens/LFG/LfgFormerScreen.tsx
+++ src/components/Screens/LFG/LfgFormerScreen.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import {LfgListScreen} from './LfgListScreen';
 
 export const LfgFormerScreen = () => {
-  return <LfgListScreen endpoint={'former'} enableFilters={false} enableReportOnly={true} />;
+  return <LfgListScreen endpoint={'former'} enableFilters={false} enableReportOnly={true} showFab={false} />;
 };
