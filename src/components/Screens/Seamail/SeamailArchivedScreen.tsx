52613b0188e1021297b626472ce105dd146d5afd
Grant Cohoe <grant@grantcohoe.com>
2025-01-22 23:27:26 -0500

    seamail archived help

--- src/components/Screens/Seamail/SeamailArchivedScreen.tsx
+++ src/components/Screens/Seamail/SeamailArchivedScreen.tsx
@@ -1,11 +1,22 @@
-import {RefreshControl} from 'react-native';
+import {RefreshControl, View} from 'react-native';
 import {SeamailFlatList} from '../../Lists/Seamail/SeamailFlatList.tsx';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useSeamailListQuery} from '../../Queries/Fez/FezQueries.ts';
 import {FezData} from '../../../libraries/Structs/ControllerStructs.tsx';
 import {AppView} from '../../Views/AppView.tsx';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {ChatStackParamList, ChatStackScreenComponents} from '../../Navigation/Stacks/ChatStackNavigator.tsx';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import {MaterialHeaderButton} from '../../Buttons/MaterialHeaderButton.tsx';
+import {AppIcons} from '../../../libraries/Enums/Icons.ts';
+import {CommonStackComponents} from '../../Navigation/CommonScreens.tsx';
 
-export const SeamailArchivedScreen = () => {
+type SeamailArchivedScreenProps = NativeStackScreenProps<
+  ChatStackParamList,
+  ChatStackScreenComponents.seamailArchivedScreen
+>;
+
+export const SeamailArchivedScreen = ({navigation}: SeamailArchivedScreenProps) => {
   const {data, refetch, isFetchingNextPage, hasNextPage, fetchNextPage, isFetching} = useSeamailListQuery({
     archived: true,
   });
@@ -16,6 +27,26 @@ export const SeamailArchivedScreen = () => {
       fetchNextPage();
     }
   };
+
+  const getNavButtons = useCallback(() => {
+    return (
+      <View>
+        <HeaderButtons left HeaderButtonComponent={MaterialHeaderButton}>
+          <Item
+            title={'Help'}
+            iconName={AppIcons.help}
+            onPress={() => navigation.push(CommonStackComponents.seamailHelpScreen)}
+          />
+        </HeaderButtons>
+      </View>
+    );
+  }, [navigation]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: getNavButtons,
+    });
+  }, [navigation, getNavButtons]);
 
   useEffect(() => {
     if (data && data.pages) {
