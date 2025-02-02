ba30325e639945dca0f10844b702a987a1f39b4c
Grant Cohoe <grant@grantcohoe.com>
2025-02-01 21:26:46 -0500

    add edit button to actual profile if its you

--- src/components/Screens/Performer/PerformerScreen.tsx
+++ src/components/Screens/Performer/PerformerScreen.tsx
@@ -6,28 +6,44 @@ import {View} from 'react-native';
 import {LoadingView} from '../../Views/Static/LoadingView.tsx';
 import {PerformerActionsMenu} from '../../Menus/Performer/PerformerActionsMenu.tsx';
 import {MaterialHeaderButton} from '../../Buttons/MaterialHeaderButton.tsx';
-import {HeaderButtons} from 'react-navigation-header-buttons';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import {CommonStackComponents} from '../../Navigation/CommonScreens.tsx';
 import {PerformerScreenBase} from './PerformerScreenBase.tsx';
+import {useUserProfileQuery} from '../../Queries/User/UserQueries.ts';
+import {AppIcons} from '../../../libraries/Enums/Icons.ts';
 
 type Props = NativeStackScreenProps<MainStackParamList, CommonStackComponents.performerScreen>;
 
 export const PerformerScreen = ({route, navigation}: Props) => {
   const {data, refetch, isFetching} = usePerformerQuery(route.params.id);
+  const {data: profilePublicData} = useUserProfileQuery();
 
   const getHeaderButtons = useCallback(() => {
     return (
       <View>
         <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          {data && profilePublicData && profilePublicData.header.userID === data?.user?.userID && (
+            <Item
+              title={'Edit'}
+              iconName={AppIcons.edit}
+              onPress={() =>
+                navigation.push(CommonStackComponents.performerEditScreen, {
+                  performerData: data,
+                })
+              }
+            />
+          )}
           <PerformerActionsMenu performerData={data} />
         </HeaderButtons>
       </View>
     );
-  }, [data]);
+  }, [data, navigation, profilePublicData]);
 
   const onRefresh = async () => {
     await refetch();
   };
+
+  console.log(data);
 
   useEffect(() => {
     navigation.setOptions({
