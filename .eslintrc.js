67bb33629bfa6e2e98fd01bf07c883306ad9bf72
Grant Cohoe <grant@grantcohoe.com>
2025-09-18 23:11:13 -0400

    yell about relative imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,16 @@ module.exports = {
   extends: ['@react-native', 'plugin:jest/recommended'],
   rules: {
     'linebreak-style': 0,
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['../src/*', './src/*', '../../src/*', '../../../src/*', '../../../../src/*'],
+            message: 'Use @tricordarr alias instead of relative imports to src directory. Replace with @tricordarr/...',
+          },
+        ],
+      },
+    ],
   },
 };
