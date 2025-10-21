f81ba8adde4a66bb208818ec8e177ff989c8a223
Grant Cohoe <grant@grantcohoe.com>
2025-10-20 23:28:33 -0400

    fixes #315

--- android/app/src/main/java/com/tricordarr/MyAppPackage.kt
+++ android/app/src/main/java/com/tricordarr/MyAppPackage.kt
@@ -7,6 +7,11 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 
+/**
+ * TODO: This is relying on an interop layer for an old-style native module.
+ * Some day we're gonna need to move to the new Turbo style modules.
+ * https://reactnative.dev/docs/turbo-native-modules-introduction
+ */
 class MyAppPackage : ReactPackage {
 
   override fun createViewManagers(reactContext: ReactApplicationContext):
