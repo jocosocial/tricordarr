3b7e90e081455f21ff873b4b409e75d9bb5f009d
Grant Cohoe <grant@grantcohoe.com>
2025-10-23 00:45:08 -0400

    smallpadding

--- src/Components/Lists/Forums/ForumPostFlatList.tsx
+++ src/Components/Lists/Forums/ForumPostFlatList.tsx
@@ -175,7 +175,7 @@ export const ForumPostFlatList = ({
       // Style is here rather than in the renderItem because the padding we use is
       // also needed for the dividers. It could be added to the divider function as
       // well but this is slightly simpler and covers cases I am not remembering.
-      style={commonStyles.paddingHorizontal}
+      style={commonStyles.paddingHorizontalSmall}
     />
   );
 };
