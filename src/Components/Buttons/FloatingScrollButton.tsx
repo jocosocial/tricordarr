30a4cbaae9e2c76dc6463327440cbde631c87589
Grant Cohoe <grant@grantcohoe.com>
2025-10-22 22:07:08 -0400

    fixes #255, makes the scroll button appear in different areas now

--- src/Components/Buttons/FloatingScrollButton.tsx
+++ src/Components/Buttons/FloatingScrollButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {IconButton} from 'react-native-paper';
 import {IconSource} from 'react-native-paper/lib/typescript/components/Icon';
 
@@ -10,30 +10,43 @@ import {FloatingScrollButtonPosition} from '#src/Types';
 interface FloatingScrollButtonProps {
   onPress: () => void;
   icon?: IconSource;
+  // Raised means there is a ContentPostView or something like that.
   displayPosition?: FloatingScrollButtonPosition;
+  small?: boolean;
 }
 
 /**
- * Button to float above content and give the user something to jump to the bottom.
+ * Button to float above content and give the user something to jump to the top/bottom.
  */
 export const FloatingScrollButton = ({
   onPress,
   icon = AppIcons.scrollDown,
   displayPosition = 'bottom',
+  small = false,
 }: FloatingScrollButtonProps) => {
   const {commonStyles, styleDefaults} = useStyles();
-  const style = {
-    ...commonStyles.flexRow,
-    ...commonStyles.justifyCenter,
-    ...commonStyles.fullWidth,
-    ...commonStyles.backgroundTransparent,
-    ...commonStyles.positionAbsolute,
-    ...(displayPosition === 'bottom' ? {bottom: styleDefaults.marginSize * 2} : undefined),
-    ...(displayPosition === 'raised' ? {bottom: styleDefaults.marginSize * 5} : undefined),
-  };
+
+  const styles = StyleSheet.create({
+    container: {
+      ...commonStyles.flexRow,
+      // These made it centered.
+      // ...commonStyles.fullWidth,
+      // ...commonStyles.justifyCenter,
+      ...commonStyles.backgroundTransparent,
+      ...commonStyles.positionAbsolute,
+      // Vertical positioning.
+      ...(displayPosition === 'bottom' ? {bottom: styleDefaults.marginSize * 1} : undefined),
+      ...(displayPosition === 'raised' ? {bottom: styleDefaults.marginSize * 4} : undefined),
+      // Horizontal positioning.
+      ...(displayPosition === 'bottom' ? {left: styleDefaults.marginSize / 2} : undefined),
+      ...(displayPosition === 'raised' ? {right: styleDefaults.marginSize / 2} : undefined),
+
+    },
+  });
+
   return (
-    <View style={style} pointerEvents={'box-none'}>
-      <IconButton icon={icon} size={30} onPress={onPress} mode={'contained-tonal'} />
+    <View style={styles.container} pointerEvents={'box-none'}>
+      <IconButton icon={icon} size={small ? 15 : 30} onPress={onPress} mode={'contained-tonal'} />
     </View>
   );
 };
