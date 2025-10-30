2aa87eb6123fc6a513968cca8e08ab03719dc621
Gowtam Lal <glal14@gmail.com>
2025-10-29 23:40:34 -0400

    Lint.

--- src/Components/Text/SeamailMessageCountIndicator.tsx
+++ src/Components/Text/SeamailMessageCountIndicator.tsx
@@ -1,21 +1,28 @@
+import pluralize from 'pluralize';
 import React from 'react';
 import {View} from 'react-native';
 import {Badge, Text} from 'react-native-paper';
 
 import {commonStyles} from '#src/Styles';
-import pluralize from 'pluralize';
 
-export const SeamailMessageCountIndicator = ({totalPostCount, badgeCount}: {totalPostCount: number, badgeCount: number}) => {
+export const SeamailMessageCountIndicator = ({
+  totalPostCount,
+  badgeCount,
+}: {
+  totalPostCount: number;
+  badgeCount: number;
+}) => {
   return (
     <View style={commonStyles.verticalContainer}>
       <View style={[commonStyles.flexRow, commonStyles.alignItemsCenter]}>
         <Text variant={'bodySmall'}>
           {totalPostCount} {pluralize('messages', totalPostCount)}
-        </Text> 
-        {
-          !!badgeCount && 
-          <Badge style={[commonStyles.marginLeftSmall, commonStyles.paddingHorizontalSmall, commonStyles.bold]}>{badgeCount} new</Badge>
-        } 
+        </Text>
+        {!!badgeCount && (
+          <Badge style={[commonStyles.marginLeftSmall, commonStyles.paddingHorizontalSmall, commonStyles.bold]}>
+            {badgeCount} new
+          </Badge>
+        )}
       </View>
     </View>
   );
