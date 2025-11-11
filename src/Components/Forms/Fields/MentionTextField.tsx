a9a4568ecf12e791445eaeb0c268fdf1e3493c6c
Grant Cohoe <grant@grantcohoe.com>
2025-11-10 23:09:29 -0500

    fix link styling

--- src/Components/Forms/Fields/MentionTextField.tsx
+++ src/Components/Forms/Fields/MentionTextField.tsx
@@ -37,10 +37,10 @@ export const MentionTextField = (props: MentionTextFieldProps) => {
       url: {
         pattern:
           /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi,
-        textStyle: {color: 'blue'},
+        textStyle: commonStyles.linkText,
       },
     }),
-    [],
+    [commonStyles.linkText],
   );
 
   const {textInputProps, triggers} = useMentions({
