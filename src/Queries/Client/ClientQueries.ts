f16c43f656bb3d189e990573f687ca3f775eab82
Grant Cohoe <grant@grantcohoe.com>
2025-11-14 00:28:31 -0500

    ensure no caching of healthcheck result

--- src/Queries/Client/ClientQueries.ts
+++ src/Queries/Client/ClientQueries.ts
@@ -1,26 +1,47 @@
-// import {useQueryClient} from '@tanstack/react-query';
+import {useQueryClient} from '@tanstack/react-query';
+import {isAxiosError} from 'axios';
 
+import {useSwiftarrQueryClient} from '#src/Context/Contexts/SwiftarrQueryClientContext';
 import {useOpenQuery, usePublicQuery} from '#src/Queries/OpenQuery';
 import {HealthResponse, SwiftarrClientConfig} from '#src/Structs/ControllerStructs';
 
 export const useHealthQuery = (options = {}) => {
-  // const client = useQueryClient();
-  // @TODO In the React Query v5 upgrade the weird anti-caching bits were taken away.
+  const client = useQueryClient();
+  const {apiGet, queryKeyExtraData} = useSwiftarrQueryClient();
+  const queryKey = ['/client/health', undefined, ...queryKeyExtraData];
+
   return useOpenQuery<HealthResponse>('/client/health', {
     retry: false,
     gcTime: 0,
     staleTime: 0,
-    // onError: response => {
-    //   // Axios gets "helpful" and throws an exception with a 500 error response. But the healthcheck
-    //   // endpoint returns something if thats the case. This forces the response data to be set if we got
-    //   // a valid healthcheck response back. It also wipes out any past data on fail because apparently
-    //   // the keepPreviousData and cacheTime above aren't enough.
-    //   if (!response.response?.data.error) {
-    //     client.setQueryData(['/client/health'], () => null);
-    //   } else {
-    //     client.setQueryData(['/client/health'], () => response.response?.data);
-    //   }
-    // },
+    refetchOnMount: 'always',
+    refetchOnWindowFocus: 'always',
+    refetchOnReconnect: 'always',
+    queryFn: async (): Promise<HealthResponse> => {
+      try {
+        const response = await apiGet<HealthResponse, undefined>('/client/health');
+        return response.data;
+      } catch (error) {
+        // Axios gets "helpful" and throws an exception with a 500 error response. But the healthcheck
+        // endpoint returns something if thats the case. This forces the response data to be set if we got
+        // a valid healthcheck response back. It also wipes out any past data on fail because apparently
+        // the keepPreviousData and cacheTime above aren't enough.
+        if (isAxiosError(error) && error.response?.data) {
+          const responseData = error.response.data as HealthResponse;
+          if (!responseData.error) {
+            // Set query data to null to wipe out past data
+            client.setQueryData(queryKey, null);
+            // Return the response data anyway (query will succeed with this data)
+            return responseData;
+          } else {
+            // Set query data explicitly and return it
+            client.setQueryData(queryKey, responseData);
+            return responseData;
+          }
+        }
+        throw error;
+      }
+    },
     ...options,
   });
 };
