a093f0636bdacd91cb52269b0420286705be8a9f
Grant Cohoe <grant@grantcohoe.com>
2025-12-20 00:28:22 -0500

    text improvements

--- src/Screens/User/UserDirectoryScreen.tsx
+++ src/Screens/User/UserDirectoryScreen.tsx
@@ -9,6 +9,8 @@ import {UserDirectoryText} from '#src/Components/Text/UserRelationsText';
 import {AppView} from '#src/Components/Views/AppView';
 import {PaddedContentView} from '#src/Components/Views/Content/PaddedContentView';
 import {ScrollingContentView} from '#src/Components/Views/Content/ScrollingContentView';
+import {HelpTopicView} from '#src/Components/Views/Help/HelpTopicView';
+import {useConfig} from '#src/Context/Contexts/ConfigContext';
 import {SwiftarrFeature} from '#src/Enums/AppFeatures';
 import {CommonStackComponents} from '#src/Navigation/CommonScreens';
 import {MainStackComponents, MainStackParamList} from '#src/Navigation/Stacks/MainStackNavigator';
@@ -28,6 +30,8 @@ export const UserDirectoryScreen = (props: Props) => {
 };
 
 const UserDirectoryScreenInner = ({navigation}: Props) => {
+  const {appConfig} = useConfig();
+
   const getNavButtons = useCallback(() => {
     return (
       <View>
@@ -43,6 +47,10 @@ const UserDirectoryScreenInner = ({navigation}: Props) => {
       headerRight: getNavButtons,
     });
   }, [navigation, getNavButtons]);
+
+  if (appConfig.preRegistrationMode) {
+    return <UserDirectoryPreRegistrationScreen />;
+  }
 
   return (
     <AppView>
@@ -61,6 +69,23 @@ const UserDirectoryScreenInner = ({navigation}: Props) => {
             clearOnPress={false}
           />
         </PaddedContentView>
+      </ScrollingContentView>
+    </AppView>
+  );
+};
+
+const UserDirectoryPreRegistrationScreen = () => {
+  return (
+    <AppView>
+      <ScrollingContentView>
+        <HelpTopicView>
+          The user directory is not available in pre-registration mode. However you can still add favorite users. Doing
+          so can enable you to call them (using The Kraken for iOS only). It will also put them in a list that you can
+          look at. Beyond that it currently means nothing.
+        </HelpTopicView>
+        <HelpTopicView>
+          To add a favorite user, tap the menu button in the top right of this screen and select "Favorite Users".
+        </HelpTopicView>
       </ScrollingContentView>
     </AppView>
   );
