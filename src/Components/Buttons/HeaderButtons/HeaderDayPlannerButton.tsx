6c5c7a9652b628ceeed627a74c51f8467c53fbb1
Gowtam Lal <glal14@gmail.com>
2025-12-22 11:24:10 -0500

    Remove this now-unused header button.

--- src/Components/Buttons/HeaderButtons/HeaderDayPlannerButton.tsx
+++
@@ -1,9 +0,0 @@
-import {useCommonStack} from '#src/Navigation/CommonScreens';
-
-interface HeaderDayPlannerButtonProps {
-  cruiseDay?: number;
-}
-
-export const HeaderDayPlannerButton = ({cruiseDay}: HeaderDayPlannerButtonProps) => {
-  const commonNavigation = useCommonStack();
-};
