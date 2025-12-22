bd5e4aa54d1099e20b12c68ab340bf17d6f6b881
Gowtam Lal <glal14@gmail.com>
2025-12-22 01:37:25 -0500

    Add native audio deps.

--- android/app/src/main/java/com/tricordarr/nativemodule/NativeTricordarrPackage.kt
+++ android/app/src/main/java/com/tricordarr/nativemodule/NativeTricordarrPackage.kt
@@ -5,14 +5,15 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.tricordarr.AudioEngineModule
 
 class NativeTricordarrPackage : BaseReactPackage() {
 
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? =
-    if (name == NativeTricordarrModule.NAME) {
-      NativeTricordarrModule(reactContext)
-    } else {
-      null
+    when (name) {
+      NativeTricordarrModule.NAME -> NativeTricordarrModule(reactContext)
+      "AudioEngine" -> AudioEngineModule(reactContext)
+      else -> null
     }
 
   override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
@@ -20,6 +21,14 @@ class NativeTricordarrPackage : BaseReactPackage() {
       NativeTricordarrModule.NAME to ReactModuleInfo(
         name = NativeTricordarrModule.NAME,
         className = NativeTricordarrModule.NAME,
+        canOverrideExistingModule = false,
+        needsEagerInit = false,
+        isCxxModule = false,
+        isTurboModule = true
+      ),
+      "AudioEngine" to ReactModuleInfo(
+        name = "AudioEngine",
+        className = "AudioEngine",
         canOverrideExistingModule = false,
         needsEagerInit = false,
         isCxxModule = false,
--- ios/NativeTricordarrModule/Tricordarr-Bridging-Header.h
+++ ios/NativeTricordarrModule/Tricordarr-Bridging-Header.h
@@ -1,11 +1,15 @@
 //
 // ObjC headers to expose to the Swift runtime.
 //
-// Unlike the inverse (Swift-to-ObjC) this is not automagically generated for us by the build process.
+// Unlike the inverse (Swift-to-ObjC) this is not automagically generated for us
+// by the build process.
 //
-// "In general" if you get compilation errors related to the auto-generated Tricordarr-Swift.h missing
-// some Apple-smelling type or whatever, it's probably because it's missing here.
+// "In general" if you get compilation errors related to the auto-generated
+// Tricordarr-Swift.h missing some Apple-smelling type or whatever, it's
+// probably because it's missing here.
 
-#import <RCTAppDelegate.h>
-#import <UserNotifications/UserNotifications.h>
 #import <NetworkExtension/NetworkExtension.h>
+#import <RCTAppDelegate.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <UserNotifications/UserNotifications.h>
