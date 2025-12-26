61f77367a50c7cd41b1df5078e902ca31f4858d9
Grant Cohoe <grant@grantcohoe.com>
2025-12-26 14:13:30 -0500

    add time debugging to ensure opening correctly

--- src/Screens/Settings/Config/TimeSettingsScreen.tsx
+++ src/Screens/Settings/Config/TimeSettingsScreen.tsx
@@ -1,15 +1,23 @@
-import {FormikHelpers} from 'formik';
-import React from 'react';
+import {Formik, FormikHelpers} from 'formik';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 
+import {BooleanField} from '#src/Components/Forms/Fields/BooleanField';
 import {TimeSettingsForm} from '#src/Components/Forms/Settings/TimeSettingsForm';
+import {ListSection} from '#src/Components/Lists/ListSection';
+import {ListSubheader} from '#src/Components/Lists/ListSubheader';
 import {AppView} from '#src/Components/Views/AppView';
 import {PaddedContentView} from '#src/Components/Views/Content/PaddedContentView';
 import {ScrollingContentView} from '#src/Components/Views/Content/ScrollingContentView';
 import {useConfig} from '#src/Context/Contexts/ConfigContext';
+import {useStyles} from '#src/Context/Contexts/StyleContext';
 import {TimeSettingsFormValues} from '#src/Types/FormValues';
 
 export const TimeSettingsScreen = () => {
   const {appConfig, updateAppConfig} = useConfig();
+  const {commonStyles} = useStyles();
+  const [forceShowTimezoneWarning, setForceShowTimezoneWarning] = useState(appConfig.forceShowTimezoneWarning);
+
   const onSubmit = (values: TimeSettingsFormValues, helpers: FormikHelpers<TimeSettingsFormValues>) => {
     updateAppConfig({
       ...appConfig,
@@ -17,6 +25,16 @@ export const TimeSettingsScreen = () => {
     });
     helpers.setSubmitting(false);
   };
+
+  const toggleForceShowTimezoneWarning = () => {
+    const newValue = !forceShowTimezoneWarning;
+    updateAppConfig({
+      ...appConfig,
+      forceShowTimezoneWarning: newValue,
+    });
+    setForceShowTimezoneWarning(newValue);
+  };
+
   const initialValues: TimeSettingsFormValues = {manualTimeOffset: appConfig.manualTimeOffset.toString()};
   return (
     <AppView>
@@ -24,6 +42,25 @@ export const TimeSettingsScreen = () => {
         <PaddedContentView padTop={true}>
           <TimeSettingsForm onSubmit={onSubmit} initialValues={initialValues} />
         </PaddedContentView>
+        {appConfig.enableDeveloperOptions && (
+          <>
+            <ListSection>
+              <ListSubheader>Developer Options</ListSubheader>
+            </ListSection>
+            <Formik initialValues={{}} onSubmit={() => {}}>
+              <View>
+                <BooleanField
+                  name={'forceShowTimezoneWarning'}
+                  label={'Force Show Timezone Warning'}
+                  onPress={toggleForceShowTimezoneWarning}
+                  value={forceShowTimezoneWarning}
+                  helperText={'Always show the timezone warning on the Today screen.'}
+                  style={commonStyles.paddingHorizontalSmall}
+                />
+              </View>
+            </Formik>
+          </>
+        )}
       </ScrollingContentView>
     </AppView>
   );
