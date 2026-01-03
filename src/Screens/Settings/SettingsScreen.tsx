b6394faa32f85b3c423b6e224e5473880e1120f7
Grant Cohoe <grant@grantcohoe.com>
2026-01-03 18:55:13 -0500

    session tweaks

--- src/Screens/Settings/SettingsScreen.tsx
+++ src/Screens/Settings/SettingsScreen.tsx
@@ -49,6 +49,13 @@ export const SettingsScreen = ({navigation}: Props) => {
               description={'Theme and styling options for this app.'}
               navComponent={CommonStackComponents.accessibilitySettingsScreen}
             />
+            {!tokenData && (
+              <SettingsNavigationListItem
+                title={'Sessions'}
+                description={'Manage local sessions.'}
+                navComponent={SettingsStackScreenComponents.sessionSettings}
+              />
+            )}
           </ListSection>
           <Divider bold={true} />
           <ListSection>
