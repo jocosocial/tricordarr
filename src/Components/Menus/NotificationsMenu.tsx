6cbb03d82409da370552b0396b18b49a8339701d
Grant Cohoe <grant@grantcohoe.com>
2026-01-18 22:37:23 -0500

    switch to real navigation

--- src/Components/Menus/NotificationsMenu.tsx
+++ src/Components/Menus/NotificationsMenu.tsx
@@ -5,9 +5,14 @@ import {Item} from 'react-navigation-header-buttons';
 
 import {AppMenu} from '#src/Components/Menus/AppMenu';
 import {usePreRegistration} from '#src/Context/Contexts/PreRegistrationContext';
-import {useTwitarr} from '#src/Context/Contexts/TwitarrContext';
 import {AppIcons} from '#src/Enums/Icons';
 import {useMenu} from '#src/Hooks/useMenu';
+import {ChatStackScreenComponents} from '#src/Navigation/Stacks/ChatStackNavigator';
+import {ForumStackComponents} from '#src/Navigation/Stacks/ForumStackNavigator';
+import {LfgStackComponents} from '#src/Navigation/Stacks/LFGStackNavigator';
+import {MainStackComponents} from '#src/Navigation/Stacks/MainStackNavigator';
+import {SettingsStackScreenComponents} from '#src/Navigation/Stacks/SettingsStackNavigator';
+import {BottomTabComponents, useBottomTabNavigator} from '#src/Navigation/Tabs/BottomTabNavigator';
 import {useUserNotificationDataQuery} from '#src/Queries/Alert/NotificationQueries';
 import {UserNotificationData} from '#src/Structs/ControllerStructs';
 
@@ -15,7 +20,7 @@ export const NotificationsMenu = () => {
   const {visible, openMenu, closeMenu} = useMenu();
   const {preRegistrationMode} = usePreRegistration();
   const {data} = useUserNotificationDataQuery({enabled: !preRegistrationMode});
-  const {openAppUrl} = useTwitarr();
+  const bottomTabNavigator = useBottomTabNavigator();
 
   const anyNew = UserNotificationData.totalNewCount(data) !== 0;
 
@@ -36,7 +41,11 @@ export const NotificationsMenu = () => {
           <Menu.Item
             title={`${data?.newAnnouncementCount} new ${pluralize('announcement', data?.newAnnouncementCount)}`}
             leadingIcon={AppIcons.notificationShow}
-            onPress={() => openAppUrl('tricordarr://home')}
+            onPress={() =>
+              bottomTabNavigator.navigate(BottomTabComponents.homeTab, {
+                screen: MainStackComponents.mainScreen,
+              })
+            }
           />
           <Divider bold={true} />
         </>
@@ -46,7 +55,11 @@ export const NotificationsMenu = () => {
           <Menu.Item
             title={`${data?.newForumMentionCount} new forum ${pluralize('mention', data?.newForumMentionCount)}`}
             leadingIcon={AppIcons.forum}
-            onPress={() => openAppUrl('tricordarr://forumpost/mentions')}
+            onPress={() =>
+              bottomTabNavigator.navigate(BottomTabComponents.forumsTab, {
+                screen: ForumStackComponents.forumPostMentionScreen,
+              })
+            }
           />
           <Divider bold={true} />
         </>
@@ -55,14 +68,24 @@ export const NotificationsMenu = () => {
         <Menu.Item
           title={`Added to ${data?.addedToSeamailCount} new ${pluralize('seamail', data?.addedToSeamailCount)}`}
           leadingIcon={AppIcons.seamail}
-          onPress={() => openAppUrl('tricordarr://seamail', {onlyNew: true})}
+          onPress={() =>
+            bottomTabNavigator.navigate(BottomTabComponents.seamailTab, {
+              screen: ChatStackScreenComponents.seamailListScreen,
+              params: {onlyNew: true},
+            })
+          }
         />
       )}
       {!!data?.newSeamailMessageCount && (
         <Menu.Item
           title={`${data?.newSeamailMessageCount} new seamail ${pluralize('message', data?.newSeamailMessageCount)}`}
           leadingIcon={AppIcons.seamail}
-          onPress={() => openAppUrl('tricordarr://seamail', {onlyNew: true})}
+          onPress={() =>
+            bottomTabNavigator.navigate(BottomTabComponents.seamailTab, {
+              screen: ChatStackScreenComponents.seamailListScreen,
+              params: {onlyNew: true},
+            })
+          }
         />
       )}
       {/* 
@@ -79,7 +102,12 @@ export const NotificationsMenu = () => {
         <Menu.Item
           title={`${data?.newFezMessageCount} new ${pluralize('LFG', data?.newFezMessageCount)} messages`}
           leadingIcon={AppIcons.lfg}
-          onPress={() => openAppUrl('tricordarr://lfg/joined', {onlyNew: true})}
+          onPress={() =>
+            bottomTabNavigator.navigate(BottomTabComponents.lfgTab, {
+              screen: LfgStackComponents.lfgJoinedScreen,
+              params: {onlyNew: true},
+            })
+          }
         />
       )}
       {/* {!!data?.addedToPrivateEventCount && (
@@ -93,14 +121,22 @@ export const NotificationsMenu = () => {
         <Menu.Item
           title={`${data?.newPrivateEventMessageCount} new private event ${pluralize('message', data?.newPrivateEventMessageCount)}`}
           leadingIcon={AppIcons.personalEvent}
-          onPress={() => openAppUrl('tricordarr://privateevent/list')}
+          onPress={() => bottomTabNavigator.navigate(BottomTabComponents.scheduleTab)}
         />
       )}
       <Divider bold={true} />
       <Menu.Item
         title={'Notification Settings'}
         leadingIcon={AppIcons.settings}
-        onPress={() => openAppUrl('tricordarr://settings/pushnotifications')}
+        onPress={() =>
+          bottomTabNavigator.navigate(BottomTabComponents.homeTab, {
+            screen: MainStackComponents.mainSettingsScreen,
+            params: {
+              screen: SettingsStackScreenComponents.pushNotificationSettings,
+              params: {},
+            },
+          })
+        }
       />
     </AppMenu>
   );
