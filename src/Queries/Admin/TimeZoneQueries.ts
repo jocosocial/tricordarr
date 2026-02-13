99f223cb460dd225e55fbc075634ace6be283a09
Grant Cohoe <grant@grantcohoe.com>
2026-02-12 20:17:53 -0500

    increase timezone stale time

--- src/Queries/Admin/TimeZoneQueries.ts
+++ src/Queries/Admin/TimeZoneQueries.ts
@@ -1,6 +1,9 @@
+import {STALE} from '#src/Libraries/Time/Time';
 import {useOpenQuery} from '#src/Queries/OpenQuery';
 import {TimeZoneChangeData} from '#src/Structs/ControllerStructs';
 
 export const useTimeZoneChangesQuery = () => {
-  return useOpenQuery<TimeZoneChangeData>('/admin/timezonechanges');
+  return useOpenQuery<TimeZoneChangeData>('/admin/timezonechanges', {
+    staleTime: STALE.HOURS.TWENTY_FOUR,
+  });
 };
