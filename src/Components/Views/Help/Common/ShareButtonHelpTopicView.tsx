14dba38b18ceb77363e436a0cb91cd1ac1b2c8d4
Grant Cohoe <grant@grantcohoe.com>
2026-02-15 12:22:27 -0500

    share long press open browser

--- src/Components/Views/Help/Common/ShareButtonHelpTopicView.tsx
+++ src/Components/Views/Help/Common/ShareButtonHelpTopicView.tsx
@@ -4,7 +4,7 @@ import {AppIcons} from '#src/Enums/Icons';
 export const ShareButtonHelpTopicView = () => {
   return (
     <HelpTopicView title={'Share'} icon={AppIcons.share}>
-      Copy the current URL to the clipboard. This can be used to share content with other users.
+      Copy the current URL to the clipboard. Long press to open the current URL in your browser.
     </HelpTopicView>
   );
 };
