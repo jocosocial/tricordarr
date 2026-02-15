cefded5f36b26241f13d27856234bed4ad84a1d5
Grant Cohoe <grant@grantcohoe.com>
2026-02-15 13:30:43 -0500

    menu scroll is now pressable

--- src/Components/Views/MenuScrollIndicator.tsx
+++ src/Components/Views/MenuScrollIndicator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 
 import {AppIcon} from '#src/Components/Icons/AppIcon';
 import {useStyles} from '#src/Context/Contexts/StyleContext';
@@ -7,13 +7,15 @@ import {AppIcons} from '#src/Enums/Icons';
 
 interface MenuScrollIndicatorProps {
   visible: boolean;
+  onPress?: () => void;
 }
 
 /**
  * A scroll indicator that appears at the bottom of scrollable menus
- * to hint to users that they can scroll down.
+ * to hint to users that they can scroll down. When onPress is provided,
+ * pressing it scrolls the menu to the bottom.
  */
-export const MenuScrollIndicator = ({visible}: MenuScrollIndicatorProps) => {
+export const MenuScrollIndicator = ({visible, onPress}: MenuScrollIndicatorProps) => {
   const {commonStyles} = useStyles();
 
   const styles = StyleSheet.create({
@@ -35,8 +37,8 @@ export const MenuScrollIndicator = ({visible}: MenuScrollIndicatorProps) => {
   }
 
   return (
-    <View style={styles.scrollIndicator}>
+    <TouchableOpacity style={styles.scrollIndicator} onPress={onPress}>
       <AppIcon icon={AppIcons.scrollDown} small />
-    </View>
+    </TouchableOpacity>
   );
 };
